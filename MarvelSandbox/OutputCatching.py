91130d543ae3ae506f419fd370266f06b4c491b2
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-06-03 11:52:36 -0500

    Pre_CC: added basic error checking

---
+++ MarvelSandbox/OutputCatching.py
@@ -0,0 +1,17 @@
+import sys
+
+sys.argv = ['']
+
+class CatchOutErr:
+
+    def __init__(self):
+        self.value = ''
+
+    def write(self, txt):
+        self.value += txt
+
+    def flush(self):
+        self.value = ''
+
+catchOutErr = CatchOutErr()
+sys.stdout = catchOutErr
