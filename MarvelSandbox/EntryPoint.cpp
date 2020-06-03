73fc755da12720bb5cd8f5daa24c551b495d24ab
Jonathan Hoffstadt <jonathan.hoffstadt@raylock.com>
2020-06-02 22:50:24 -0500

    Pre_CC: got sandbox ready to be runnable

--- MarvelSandbox/EntryPoint.cpp
+++ MarvelSandbox/EntryPoint.cpp
@@ -5,20 +5,43 @@
 
 using namespace Marvel;
 
-int main()
+int main(int argc, char* argv[])
 {
 	mvWindow* window = new mvWindowsWindow();
 
 	window->show();
+	if(argc < 2)
+		Py_SetPath(L"python38.zip;../../../../MarvelSandbox");
+	else if(argc == 2)
+		Py_SetPath(L"python38.zip;");
+	else
+	{
+		// Convert to a wchar_t*
+		size_t origsize = strlen(argv[1]) + 1;
+		const size_t newsize = 100;
+		size_t convertedChars = 0;
+		wchar_t wcstring[newsize];
+		mbstowcs_s(&convertedChars, wcstring, origsize, argv[1], _TRUNCATE);
+		//wcscat_s(wcstring, L" (wchar_t *)");
+		std::wstring path = std::wstring(L"python38.zip;") + std::wstring(wcstring);
+		std::wcout << path << std::endl;
+		Py_SetPath(path.c_str());
+	}
+		
 
-	Py_SetPath(L"python38.zip;../../../../MarvelSandbox");
 	PyImport_AppendInittab("sandbox", &PyInit_emb);
 
 	// start the intpreter
 	Py_Initialize();
 
 	// import the module (python file) looking in the path
-	PyObject* pModule = PyImport_ImportModule("App"); // new reference
+	PyObject* pModule;
+	if (argc < 2)
+		pModule = PyImport_ImportModule("App"); // new reference
+	else if (argc == 2)
+		pModule = PyImport_ImportModule(argv[1]); // new reference
+	else
+		pModule = PyImport_ImportModule(argv[2]); // new reference
 
 	// returns the dictionary object representing the module namespace
 	PyObject* pDict = PyModule_GetDict(pModule); // borrowed reference
