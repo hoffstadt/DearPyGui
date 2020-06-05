f4d1c5d5c41f87df9c748cdbf18aa54743103a3e
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-06-05 11:45:33 -0500

    Pre_CC: combined labeltext and text

--- MarvelSandbox/src/Core/AppItems/mvText.h
+++ MarvelSandbox/src/Core/AppItems/mvText.h
@@ -4,6 +4,9 @@
 
 namespace Marvel {
 
+	//-----------------------------------------------------------------------------
+	// mvLabelText
+	//-----------------------------------------------------------------------------
 	class mvText : public mvNoneItemBase
 	{
 
@@ -38,6 +41,50 @@ namespace Marvel {
 
 		mvColor m_color;
 		int     m_wrap;
+
+	};
+
+	//-----------------------------------------------------------------------------
+	// mvLabelText
+	//-----------------------------------------------------------------------------
+	class mvLabelText : public mvStringItemBase
+	{
+
+	public:
+
+		MV_APPITEM_TYPE(mvAppItemType::LabelText)
+
+			mvLabelText(const std::string& parent, const std::string& name, const std::string& value, int wrap, mvColor color)
+			: mvStringItemBase(parent, name, value), m_wrap(wrap), m_color(color)
+		{
+		}
+
+		virtual void draw() override
+		{
+
+			if (m_wrap != 0)
+				ImGui::PushTextWrapPos(ImGui::GetCursorPos().x + m_wrap);
+
+			if (m_color.specified)
+				ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(m_color.r, m_color.g, m_color.b, m_color.a));
+
+			ImGui::Text(m_value.c_str());
+
+			if (m_color.specified)
+				ImGui::PopStyleColor();
+
+			ImGui::SameLine();
+			ImGui::Text(m_label.c_str());
+
+			if (m_wrap != 0)
+				ImGui::PopTextWrapPos();
+
+		}
+
+	private:
+
+		mvColor      m_color;
+		int          m_wrap;
 
 	};
 
