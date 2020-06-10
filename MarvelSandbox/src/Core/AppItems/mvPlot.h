804edc44d016edb71cc93dc7f654412eaab755ab
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-06-10 12:38:45 -0500

    Got marvelgraph basics working

--- MarvelSandbox/src/Core/AppItems/mvPlot.h
+++ MarvelSandbox/src/Core/AppItems/mvPlot.h
@@ -32,50 +32,37 @@ namespace Marvel {
 
 			m_graph = new marvel::mvImGuiGraph();
 
-			// create data for the plot
-			std::vector<marvel::mvPoint> dataset1;
-			dataset1.emplace_back(0.0, 0.0);
-			dataset1.emplace_back(1.0, 1.0);
-			dataset1.emplace_back(2.0, 1.0);
-
-			std::vector<marvel::mvPoint> dataset2;
-			dataset2.emplace_back(-6.0, 3.0);
-			dataset2.emplace_back(0.0, -3.0);
-			dataset2.emplace_back(3.0, 0.0);
-
-			// create a line series
-			marvel::mvSeries* lineseries1 = new marvel::mvLineSeries(m_graph, "Series1", dataset1, mvGREEN);
-			marvel::mvSeries* lineseries2 = new marvel::mvLineSeries(m_graph, "Series2", dataset2, mvRED);
-
-			// add series
-			m_graph->addSeries(lineseries1);
-			m_graph->addSeries(lineseries2);
-
 			//// (optional) add an input poller for additional interactions (more worked needed here)
 			m_graph->addInputPoller(new marvel::mvImguiInput());
 
-			m_graph->OnEvent(marvel::mvWindowResizeEvent(m_width, m_height));
+			m_graph->setSize(width, height);
+
 		}
 
 		virtual void draw() override
 		{
 
-
 			int m_cposx = (int)ImGui::GetCursorScreenPos().x;
 			int m_cposy = (int)ImGui::GetCursorScreenPos().y;
-			//int m_width = ImGui::GetWindowWidth()*0.5;
-			//int m_height = ImGui::GetWindowHeight()*0.5;
 
 			// set starting point
 			m_graph->setStart(m_cposx, m_cposy);
 
-			if(m_width != m_graph->getWidth() || m_height != m_graph->getHeight())
+			if (m_width != m_graph->getWidth() || m_height != m_graph->getHeight())
+			{
+				m_width = m_graph->getWidth();
+				m_height = m_graph->getHeight();
 				m_graph->OnEvent(marvel::mvWindowResizeEvent(m_width, m_height));
+			}
+				
 
 			// must be called since imgui updates every frame
 			m_graph->OnEvent(marvel::mvAppRenderEvent());
 
 		}
+
+		inline marvel::mvImGuiGraph* getGraph() { return m_graph; }
+		inline void setHeight(int height) { m_height = height; }
 
 	private:
 
--- MarvelSandbox/src/Core/PythonInterfaces/mvPlotInterface.cpp
+++ MarvelSandbox/src/Core/PythonInterfaces/mvPlotInterface.cpp
@@ -25,9 +25,70 @@ namespace Marvel {
 		return Py_None;
 	}
 
+	PyObject* addSeries(PyObject* self, PyObject* args, PyObject* kwargs)
+	{
+		const char* plot;
+		const char* name;
+		const char* seriestype;
+		PyObject* data;
+		PyObject* color;
+
+		auto pl = mvPythonTranslator(args, kwargs, {
+			mvPythonDataElement(mvPythonDataType::String, "plot"),
+			mvPythonDataElement(mvPythonDataType::String, "name"),
+			mvPythonDataElement(mvPythonDataType::String, "seriestype"),
+			mvPythonDataElement(mvPythonDataType::FloatList, "data"),
+			mvPythonDataElement(mvPythonDataType::FloatList, "color")
+			});
+
+		pl.parse(__FUNCTION__, &plot, &name, &seriestype, &data, &color);
+
+		auto pcolor = pl.getColor(color);
+
+		mvAppItem* aplot = mvApp::GetApp()->getItem(plot);
+		marvel::mvImGuiGraph* graph = static_cast<mvPlot*>(aplot)->getGraph();
+
+		auto datapoints = pl.getVectVec2L(data);
+		std::vector<marvel::mvPoint> points;
+		for (const auto& point : datapoints)
+			points.emplace_back(point.x, point.y);
+
+		marvel::mvSeries* lineseries1 = new marvel::mvLineSeries(graph, name, points, {(int)pcolor.r*255, (int)pcolor.g*255, (int)pcolor.b*255, (int)pcolor.a*255});
+
+		graph->addSeries(lineseries1);
+
+		Py_INCREF(Py_None);
+		return Py_None;
+	}
+
+	PyObject* changePlotSize(PyObject* self, PyObject* args, PyObject* kwargs)
+	{
+		const char* plot;
+		int width;
+		int height;
+
+		auto pl = mvPythonTranslator(args, kwargs, {
+			mvPythonDataElement(mvPythonDataType::String, "plot"),
+			mvPythonDataElement(mvPythonDataType::Integer, "width"),
+			mvPythonDataElement(mvPythonDataType::Integer, "height")
+			});
+
+		pl.parse(__FUNCTION__, &plot, &width, &height);
+
+		mvPlot* aplot = static_cast<mvPlot*>(mvApp::GetApp()->getItem(plot));
+
+		aplot->getGraph()->setSize(width, height);
+
+
+		Py_INCREF(Py_None);
+		return Py_None;
+	}
+
 	void CreatePlotInterface(mvPythonModule& pyModule, PyObject* (*initfunc)())
 	{
 		pyModule.addMethod(addPlot, "Not Documented");
+		pyModule.addMethod(addSeries, "Not Documented");
+		pyModule.addMethod(changePlotSize, "Not Documented");
 
 		PyImport_AppendInittab(pyModule.getName(), initfunc);
 	}
