02e650e053065918cfc32e93097d1246d27dc076
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-06-22 13:48:38 -0500

    Pre_CC: added item query settings back

--- MarvelSandbox/src/Core/AppItems/mvPopup.h
+++ MarvelSandbox/src/Core/AppItems/mvPopup.h
@@ -54,6 +54,21 @@ namespace Marvel {
 						item->draw();
 						item->popColorStyles();
 
+
+						item->setHovered(ImGui::IsItemHovered());
+						item->setActive(ImGui::IsItemActive());
+						item->setFocused(ImGui::IsItemFocused());
+						item->setClicked(ImGui::IsItemClicked());
+						item->setVisible(ImGui::IsItemVisible());
+						item->setEdited(ImGui::IsItemEdited());
+						item->setActivated(ImGui::IsItemActivated());
+						item->setDeactivated(ImGui::IsItemDeactivated());
+						item->setDeactivatedAfterEdit(ImGui::IsItemDeactivatedAfterEdit());
+						item->setToggledOpen(ImGui::IsItemToggledOpen());
+						item->setRectMin({ ImGui::GetItemRectMin().x, ImGui::GetItemRectMin().y });
+						item->setRectMax({ ImGui::GetItemRectMax().x, ImGui::GetItemRectMax().y });
+						item->setRectSize({ ImGui::GetItemRectSize().x, ImGui::GetItemRectSize().y });
+
 					}
 
 					ImGui::EndPopup();
