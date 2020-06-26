bc77a64bd74891514c9c3223c9cd01fb372275ef
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-06-25 19:25:10 -0500

    Pre_CC: fixed a few issues

--- Scripts/PackageWindowsApp.py
+++ Scripts/PackageWindowsApp.py
@@ -10,12 +10,14 @@ build_dir = script_dir + "/../Dependencies/cpython/PCbuild/amd64/"
 
 # directories to put prepared files into
 window_app_dir = script_dir + "/../Output/WindowsApp/"
+app_depend_dir = script_dir + "/../Output/WindowsApp/Dependencies/"
 
 new_python_dir = script_dir + "/../Output/Python/"
 debug_dir = script_dir + "/../Output/Python/Debug/"
 
 if not os.path.isdir(window_app_dir):
     os.mkdir(window_app_dir)
+    os.mkdir(app_depend_dir)
 
 # delete everything except python fils
 for file in glob.glob("../WindowsApp/*.*"):
@@ -24,52 +26,52 @@ for file in glob.glob("../WindowsApp/*.*"):
 # copy everything to windows package folder
 ##########################################################
 
-shutil.copy(build_dir + "/python.exe", window_app_dir)
-shutil.copy(build_dir + "/pythonw.exe", window_app_dir) 
-shutil.copy(build_dir + "/_asyncio.pyd", window_app_dir)
-shutil.copy(build_dir + "/_bz2.pyd", window_app_dir)
-shutil.copy(build_dir + "/_ctypes.pyd", window_app_dir)
-shutil.copy(build_dir + "/_ctypes_test.pyd", window_app_dir)
-shutil.copy(build_dir + "/_decimal.pyd", window_app_dir)
-shutil.copy(build_dir + "/_elementtree.pyd", window_app_dir)
-shutil.copy(build_dir + "/_hashlib.pyd", window_app_dir)
-shutil.copy(build_dir + "/_lzma.pyd", window_app_dir)
-shutil.copy(build_dir + "/_msi.pyd", window_app_dir)
-shutil.copy(build_dir + "/_multiprocessing.pyd", window_app_dir)
-shutil.copy(build_dir + "/_overlapped.pyd", window_app_dir)
-shutil.copy(build_dir + "/_queue.pyd", window_app_dir)
-shutil.copy(build_dir + "/_socket.pyd", window_app_dir)
-shutil.copy(build_dir + "/_sqlite3.pyd", window_app_dir)
-shutil.copy(build_dir + "/_ssl.pyd", window_app_dir)
-shutil.copy(build_dir + "/_testbuffer.pyd", window_app_dir)
-shutil.copy(build_dir + "/_testcapi.pyd", window_app_dir)
-shutil.copy(build_dir + "/_testconsole.pyd", window_app_dir)
-shutil.copy(build_dir + "/_testimportmultiple.pyd", window_app_dir)
-shutil.copy(build_dir + "/_testinternalcapi.pyd", window_app_dir)
-shutil.copy(build_dir + "/_testmultiphase.pyd", window_app_dir)
-shutil.copy(build_dir + "/_tkinter.pyd", window_app_dir)
-shutil.copy(build_dir + "/libcrypto-1_1.dll", window_app_dir)
-shutil.copy(build_dir + "/libffi-7.dll", window_app_dir)
-shutil.copy(build_dir + "/pyexpat.pyd", window_app_dir)
-shutil.copy(build_dir + "/pyshellext.dll", window_app_dir)
-shutil.copy(build_dir + "/python3.dll", window_app_dir)
-shutil.copy(build_dir + "/python38.dll", window_app_dir)
-shutil.copy(build_dir + "/select.pyd", window_app_dir)
-shutil.copy(build_dir + "/sqlite3.dll", window_app_dir)
-shutil.copy(build_dir + "/tcl86t.dll", window_app_dir)
-shutil.copy(build_dir + "/tk86t.dll", window_app_dir)
-shutil.copy(build_dir + "/unicodedata.pyd", window_app_dir)
-shutil.copy(build_dir + "/vcruntime140.dll", window_app_dir)
-shutil.copy(build_dir + "/vcruntime140_1.dll", window_app_dir)
-shutil.copy(build_dir + "/winsound.pyd", window_app_dir)
-shutil.copy(build_dir + "/xxlimited.pyd", window_app_dir)
-shutil.copy("../cmake-build-release/MarvelSandbox/MarvelSandbox.exe", window_app_dir)
-shutil.copy("../cmake-build-release/MarvelSandbox/MarvelSandbox.exe.manifest", window_app_dir)
-shutil.copy("../AppSamples/App.py", "../Output/WindowsApp/")
-shutil.copy(debug_dir + "/python38.zip", window_app_dir)
+shutil.copy(build_dir + "/python.exe", app_depend_dir)
+shutil.copy(build_dir + "/pythonw.exe", app_depend_dir) 
+shutil.copy(build_dir + "/_asyncio.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_bz2.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_ctypes.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_ctypes_test.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_decimal.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_elementtree.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_hashlib.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_lzma.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_msi.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_multiprocessing.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_overlapped.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_queue.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_socket.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_sqlite3.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_ssl.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_testbuffer.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_testcapi.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_testconsole.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_testimportmultiple.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_testinternalcapi.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_testmultiphase.pyd", app_depend_dir)
+shutil.copy(build_dir + "/_tkinter.pyd", app_depend_dir)
+shutil.copy(build_dir + "/libcrypto-1_1.dll", app_depend_dir)
+shutil.copy(build_dir + "/libffi-7.dll", app_depend_dir)
+shutil.copy(build_dir + "/pyexpat.pyd", app_depend_dir)
+shutil.copy(build_dir + "/pyshellext.dll", app_depend_dir)
+shutil.copy(build_dir + "/python3.dll", app_depend_dir)
+shutil.copy(build_dir + "/python38.dll", app_depend_dir)
+shutil.copy(build_dir + "/select.pyd", app_depend_dir)
+shutil.copy(build_dir + "/sqlite3.dll", app_depend_dir)
+shutil.copy(build_dir + "/tcl86t.dll", app_depend_dir)
+shutil.copy(build_dir + "/tk86t.dll", app_depend_dir)
+shutil.copy(build_dir + "/unicodedata.pyd", app_depend_dir)
+shutil.copy(build_dir + "/vcruntime140.dll", app_depend_dir)
+shutil.copy(build_dir + "/vcruntime140_1.dll", app_depend_dir)
+shutil.copy(build_dir + "/winsound.pyd", app_depend_dir)
+shutil.copy(build_dir + "/xxlimited.pyd", app_depend_dir)
+shutil.copy("../cmake-build-release/MarvelSandbox/MarvelSandbox.exe", app_depend_dir)
+shutil.copy("../cmake-build-release/MarvelSandbox/MarvelSandbox.exe.manifest", app_depend_dir)
+shutil.copy("../AppSamples/App.py", app_depend_dir)
+shutil.copy(debug_dir + "/python38.zip", app_depend_dir)
 
 # add python path file for embedding
-with open(window_app_dir + "/python38._pth", 'w') as file:
+with open(app_depend_dir + "/python38._pth", 'w') as file:
     file.write("python38.zip\n")
     file.write("Lib\n")
     file.write(".\n")
@@ -77,11 +79,11 @@ with open(window_app_dir + "/python38._pth", 'w') as file:
     file.write("#import site\n")
 
 # add App batch file
-with open(window_app_dir + "/RunApp.bat", 'w') as file:
+with open(app_depend_dir + "/RunApp.bat", 'w') as file:
     file.write("@echo off\n")
     file.write("call MarvelSandbox.exe --app App\n")
 
 # add Editor batch file
-with open(window_app_dir + "/RunEditor.bat", 'w') as file:
+with open(app_depend_dir + "/RunEditor.bat", 'w') as file:
     file.write("@echo off\n")
     file.write("call MarvelSandbox.exe --editor\n")
