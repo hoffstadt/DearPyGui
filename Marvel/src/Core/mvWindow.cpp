ef75cac5be711c508492e001cee00ac143726fc4
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-07-01 09:30:52 -0500

    Pre_CC: fixed render loop bug

--- Marvel/src/Core/mvWindow.cpp
+++ Marvel/src/Core/mvWindow.cpp
@@ -11,20 +11,31 @@ namespace Marvel {
 			prerender();
 			
 			if (m_error)
+			{
+				m_logger->prerender();
 				m_logger->render(m_error);
+				m_logger->postrender();
+			}
 
 			else if (m_editor)
+			{
+				m_appEditor->prerender();
 				m_appEditor->render(m_editor);
+				m_appEditor->postrender();
+			}
 
 			else if(m_doc)
+			{
+				m_documentation->prerender();
 				m_documentation->render(m_doc);
+				m_documentation->postrender();
+			}
 
 			else if (!m_error)
 			{
 				m_app->prerender();
 				m_app->render(m_running);
 				m_app->postrender();
-
 			}
 
 			postrender();
