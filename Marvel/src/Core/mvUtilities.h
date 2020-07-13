82656ea70e36e8abd66f31446e7d3b1f80b8c6f8
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-07-13 14:10:27 -0500

    added run_file command

--- Marvel/src/Core/mvUtilities.h
+++ Marvel/src/Core/mvUtilities.h
@@ -6,7 +6,7 @@
 namespace Marvel {
 
 	bool        LoadTextureFromFile(const char* filename, void* vout_srv, int* out_width, int* out_height);
-	void        RunFile            (const std::string& name, const std::string& file, const std::string& flags);
+	void        RunFile            (const std::string& name, const std::string& file, const std::string& flags = "");
 	std::string OpenFile           (const std::vector<std::pair<std::string, std::string>>& extensions);
 	std::string SaveFile           (const std::vector<std::pair<std::string, std::string>>& extensions);
 
