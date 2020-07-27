525478bde47e524be1715950c727a34f4457218c
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-07-27 11:07:01 -0500

    Pre_CC: v0.1.0a20

--- Distribution/setup.py
+++ Distribution/setup.py
@@ -14,7 +14,7 @@ class BinaryDistribution(Distribution):
 
 setup(
     name='dearpygui',  # Required
-    version='0.1.0a19',  # Required
+    version='0.1.0a20',  # Required
     description='DearPyGui',  # Required
     long_description=long_description,  # Optional
     long_description_content_type='text/markdown',  # Optional (see note above)
