c048e2a793763e797ecd85be6144ccf7259b58b9
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-07-28 13:46:51 -0500

    Pre_CC: fixed color edit data storage bug

--- DearSandbox/Demo.py
+++ DearSandbox/Demo.py
@@ -305,6 +305,102 @@ def InputPollingMainCallback(sender, data):
 
     set_value("Mouse Position", str(get_mouse_pos()))
 
+    # keys
+    if is_key_down(mvKey_A):
+        set_value("A key Down", "True")
+    else:
+        set_value("A key Down", "False")
+
+    if is_key_pressed(mvKey_W):
+        set_value("W key Pressed", "True")
+    else:
+        set_value("W key Pressed", "False")
+
+    if is_key_released(mvKey_Q):
+        set_value("Q key Released", "True")
+    else:
+        set_value("Q key Released", "False")
+
+    # mouse dragging
+    if is_mouse_button_dragging(mvMouseButton_Left, 10):
+        set_value("Left Mouse Dragging", "True")
+    else:
+        set_value("Left Mouse Dragging", "False")
+
+    if is_mouse_button_dragging(mvMouseButton_Right, 10):
+        set_value("Right Mouse Dragging", "True")
+    else:
+        set_value("Right Mouse Dragging", "False")
+
+    if is_mouse_button_dragging(mvMouseButton_Middle, 10):
+        set_value("Middle Mouse Dragging", "True")
+    else:
+        set_value("Middle Mouse Dragging", "False")
+
+    # mouse down
+    if is_mouse_button_down(mvMouseButton_Left):
+        set_value("Left Mouse Down", "True")
+    else:
+        set_value("Left Mouse Down", "False")
+
+    if is_mouse_button_down(mvMouseButton_Right):
+        set_value("Right Mouse Down", "True")
+    else:
+        set_value("Right Mouse Down", "False")
+
+    if is_mouse_button_down(mvMouseButton_Middle):
+        set_value("Middle Mouse Down", "True")
+    else:
+        set_value("Middle Mouse Down", "False")
+
+    # mouse clicked
+    if is_mouse_button_clicked(mvMouseButton_Left):
+        set_value("Left Mouse Clicked", "True")
+    else:
+        set_value("Left Mouse Clicked", "False")
+
+    if is_mouse_button_clicked(mvMouseButton_Right):
+        set_value("Right Mouse Clicked", "True")
+    else:
+        set_value("Right Mouse Clicked", "False")
+
+    if is_mouse_button_clicked(mvMouseButton_Middle):
+        set_value("Middle Mouse Clicked", "True")
+    else:
+        set_value("Middle Mouse Clicked", "False")
+
+    # mouse double clicked
+    if is_mouse_button_double_clicked(mvMouseButton_Left):
+        set_value("Left Mouse Double Clicked", "True")
+    else:
+        set_value("Left Mouse Double Clicked", "False")
+
+    if is_mouse_button_double_clicked(mvMouseButton_Right):
+        set_value("Right Mouse Double Clicked", "True")
+    else:
+        set_value("Right Mouse Double Clicked", "False")
+
+    if is_mouse_button_double_clicked(mvMouseButton_Middle):
+        set_value("Middle Mouse Double Clicked", "True")
+    else:
+        set_value("Middle Mouse Double Clicked", "False")
+
+    # mouse released
+    if is_mouse_button_released(mvMouseButton_Left):
+        set_value("Left Mouse Released", "True")
+    else:
+        set_value("Left Mouse Released", "False")
+
+    if is_mouse_button_released(mvMouseButton_Right):
+        set_value("Right Mouse Released", "True")
+    else:
+        set_value("Right Mouse Released", "False")
+
+    if is_mouse_button_released(mvMouseButton_Middle):
+        set_value("Middle Mouse Released", "True")
+    else:
+        set_value("Middle Mouse Released", "False")
+
 def colormapCallback(sender, data):
     value = get_value("Colormaps")
     set_color_map("Plot", value)
