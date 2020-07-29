78aa408a6835b374a85398b88ba152c54ebd5dbd
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-07-29 11:18:50 -0500

    Pre_CC: testing ci

--- Distribution/Apple/DistributeTestWheel.sh
+++ Distribution/Apple/DistributeTestWheel.sh
@@ -1,3 +1,3 @@
 python3 "BuildPythonWheel.py"
-python3 -m setup bdist_wheel --plat-name macosx-10.6-x86_64
+python3 -m setup bdist_wheel --plat-name macosx-10.6-x86_64 --dist-dir ../../dist
 #python3 -m twine upload --repository testpypi dist/*
