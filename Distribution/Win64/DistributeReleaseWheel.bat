40039504156fedb2dd2cc91b3321e0792519e3c8
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-07-29 10:51:08 -0500

    ci testing

--- Distribution/Win64/DistributeReleaseWheel.bat
+++ Distribution/Win64/DistributeReleaseWheel.bat
@@ -4,7 +4,7 @@ echo Running python preparation script
 call "%dir%..\..\Dependencies\cpython\PCbuild\amd64\python.exe" "%dir%..\..\Distribution\Win64\BuildPythonWheel.py"
 
 echo Building DearPyGui Wheel
-call "%dir%..\..\Dependencies\cpython\PCbuild\amd64\python.exe" -m setup bdist_wheel --plat-name win_amd64
+call "%dir%..\..\Dependencies\cpython\PCbuild\amd64\python.exe" -m setup bdist_wheel --plat-name win_amd64 --dist-dir ../../dist
 
 echo Building DearPyGui Wheel
 call "%dir%..\..\Dependencies\cpython\PCbuild\amd64\python.exe" -m twine upload dist/*
