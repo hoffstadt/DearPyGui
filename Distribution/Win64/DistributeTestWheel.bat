4f532b06716cb77c6022b7bd1cbe0d0a6da34b94
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-07-29 12:43:02 -0500

    readying merge

--- Distribution/Win64/DistributeTestWheel.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo Running python preparation script
-call "%dir%..\..\Dependencies\cpython\PCbuild\amd64\python.exe" "%dir%..\..\Distribution\Win64\BuildPythonWheel.py"
-
-echo Building DearPyGui Wheel
-call "%dir%..\..\Dependencies\cpython\PCbuild\amd64\python.exe" -m setup bdist_wheel --plat-name win_amd64 --dist-dir ../../dist
-
-pause
