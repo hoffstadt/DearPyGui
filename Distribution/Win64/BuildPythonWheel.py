10a9cf7b929ce52a74fd619d8b7f232c0763531b
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-07-29 14:03:01 -0500

    Pre_CC: fixed distrubution bug

--- Distribution/Win64/BuildPythonWheel.py
+++ Distribution/Win64/BuildPythonWheel.py
@@ -8,7 +8,7 @@ script_dir = os.getcwd()
 if not os.path.isdir(script_dir + "/dearpygui/"):
     os.mkdir(script_dir + "/dearpygui/")
 
-shutil.copy(script_dir + "/../../cmake-build-release/DearPyGui/dearpygui.pyd", script_dir + "/dearpygui")
+shutil.copy(script_dir + "/../../cmake-build-release/DearPyGui/Release/dearpygui.pyd", script_dir + "/dearpygui")
 shutil.copy(script_dir + "/../../DearPyGui/stubs/dearpygui.pyi", script_dir + "/dearpygui")
 
 with open(script_dir + "/dearpygui/__init__.py", 'w') as file:
