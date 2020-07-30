04fd0c1dc67e6ffa80d81aa65ff414a59ebee497
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-07-30 18:58:35 -0500

    Pre_CC: basic imgui working

---
+++ ImguiTesting/oldmain.cpp
@@ -0,0 +1,22 @@
+﻿#include "mvWindowsWindow.h"
+#include <implot.h>
+
+int main()
+{
+	mvWindowsWindow* window = new mvWindowsWindow();
+
+	window->show();
+
+	window->setup();
+	while (window->m_running)
+	{
+		window->prerender();
+
+		ImGui::ShowDemoWindow();
+
+		ImPlot::ShowDemoWindow();
+
+		window->postrender();
+	}
+
+}
