ec1f3e77bb8e5dd9e5c2fa5263646f77b09e3b64
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-07-30 23:13:30 -0500

    basic working

--- ImguiTesting/src/mvLinuxWindow.h
+++ ImguiTesting/src/mvLinuxWindow.h
@@ -1,24 +1,22 @@
 #pragma once
 
-#import <Metal/Metal.h>
-#import <QuartzCore/QuartzCore.h>
-
 class GLFWwindow;
 
-class mvAppleWindow
+class mvLinuxWindow
 {
 
 public:
 
     bool m_running = true;
 
-    mvAppleWindow();
+    mvLinuxWindow();
 
     virtual void show() {}
 
     virtual void setup();
     virtual void prerender();
     virtual void render();
+    virtual void postrender();
     virtual void cleanup();
 
 private:
@@ -26,8 +24,5 @@ private:
     float m_clear_color[4] = {0.45f, 0.55f, 0.60f, 1.00f};
 
     GLFWwindow*              m_window;
-    CAMetalLayer*            m_layer;
-    MTLRenderPassDescriptor* m_renderPassDescriptor;
-    id <MTLCommandQueue>     m_commandQueue;
 
 };
