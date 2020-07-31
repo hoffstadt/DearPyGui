d62d803252e8e5b1c26d4d65d0a3055a794679ac
RaylockLLC <jonathanhoffstadt@yahoo.com>
2020-07-31 00:06:29 -0500

    added ubuntu workflow6

--- ImguiTesting/main.mm
+++ ImguiTesting/main.mm
@@ -1,16 +1,1 @@
-#include "mvAppleWindow.h"
-#include <implot.h>
-
-int main()
-{
-    auto window = new mvAppleWindow();
-
-    window->show();
-
-    window->setup();
-    while (window->m_running)
-        window->prerender();
-
-    window->cleanup();
-
-}
+#include "main.cpp"
