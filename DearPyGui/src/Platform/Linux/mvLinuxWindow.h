2e0823fa47c97e425f393a96e5d7013f32230cdb
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-08-02 14:56:11 -0500

    fixed linux window issue

--- DearPyGui/src/Platform/Linux/mvLinuxWindow.h
+++ DearPyGui/src/Platform/Linux/mvLinuxWindow.h
@@ -22,16 +22,6 @@ namespace Marvel {
 
 	private:
         float             m_clear_color[4] = {0.45f, 0.55f, 0.60f, 1.00f};
-		bool              m_running = true;
-		bool              m_editor = false;
-		bool              m_error = false;
-		bool              m_doc = false;
-		mvStandardWindow* m_app = nullptr;
-		mvStandardWindow* m_appEditor = nullptr;
-		mvStandardWindow* m_documentation = nullptr;
-		unsigned          m_width;
-		unsigned          m_height;
-        GLFWwindow*       m_window;
 
 	};
 
