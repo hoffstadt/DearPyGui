fcac9c36b8aec82134218e777a9526e05e4c467e
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-08-03 09:54:04 -0500

    Pre_CC: removed json

--- DearSandbox/main.cpp
+++ DearSandbox/main.cpp
@@ -6,9 +6,7 @@
 #include <Windows.h>
 #include <iostream>
 #include <fstream>
-#include <nlohmann/json.hpp>
 #include <filesystem>
-#include <CLI11.hpp>
 
 namespace fs = std::filesystem;
 using namespace Marvel;
@@ -16,65 +14,6 @@ using namespace Marvel;
 int main(int argc, char* argv[])
 {
 	mvApp::GetApp()->setArgv0(argv[0]);
-
-	wchar_t* program;
-	bool errorMode = false;
-	PyObject* m = nullptr;
-	std::string AppName = "Demo";
-	std::string PathName = "";
-	bool documentation = false; // starts application with the documentation window shown
-	bool editorMode = false; // starts application in editor mode
-	bool ignoreConfig = false;
-
-	CLI::App app{ "Dear Sandbox" };
-	app.allow_windows_style_options();
-
-	// options
-	app.add_option("-a, --app", AppName, "Name of the python file (without extension)");
-	app.add_option("-p, --path", PathName, "Path to app file (default is location of MarvelSandbox.exe)");
-
-	// flags
-	app.add_flag("-d, --documentation", documentation, "Sets MarvelSandbox to Documentation Mode");
-	app.add_flag("-e, --editor", editorMode, "Sets MarvelSandbox to Editor Mode");
-	app.add_flag("-n, --noconfig", ignoreConfig, "Ignores config file");
-
-	CLI11_PARSE(app, argc, argv);
-
-	// no commandline arguments, so use config file
-	if (!ignoreConfig)
-	{
-		std::ifstream stream("dearpygui_config.json");
-		if (stream.good())
-		{
-			nlohmann::json j = nlohmann::json::parse(stream);
-
-			if (j.contains("App") && !AppName.empty())
-			{
-				AppName = j["App"];
-			}
-			else if (AppName.empty())
-				AppName = "Demo";
-
-			if (j.contains("Mode"))
-			{
-
-				std::string mode = j["Mode"];
-				if (mode == "Documentation")
-					documentation = true;
-				else if (mode == "Editor")
-					editorMode = true;
-			}
-
-			if (j.contains("Theme"))
-				mvApp::GetApp()->setAppTheme(j["Theme"]);
-
-			if (j.contains("Path"))
-				PathName = std::string(j["Path"]);
-
-		}
-	}
-
-
 
 #ifdef MV_RELEASE
 	HWND hWnd = GetConsoleWindow();
@@ -84,31 +23,17 @@ int main(int argc, char* argv[])
 	ShowWindow(hWnd, SW_SHOW);
 #endif
 
-	// handle paths
-	auto dependencies = PathName + "/Dependencies/;";
-	auto zipdependencies = PathName + "/Dependencies/python38.zip;";
-
-	std::string path = PathName + ";" + dependencies + zipdependencies + "./Dependencies/;" + "./Dependencies/python38.zip;";
-
-	program = Py_DecodeLocale(argv[0], NULL);
-	if (program == NULL) {
-		fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
-		exit(1);
-	}
-	Py_SetProgramName(program);  /* optional but recommended */
-
-	// initialize python
 	// add our custom module
 	PyImport_AppendInittab("sandboxout", &PyInit_embOut);
 	PyImport_AppendInittab("dearpygui", &PyInit_dearpygui);
 
 	// set path and start the interpreter
-	wchar_t* deco = Py_DecodeLocale(path.c_str(), nullptr);
-	Py_SetPath(deco);
+	wchar_t* path = Py_DecodeLocale("../../DearSandbox/;./Dependencies/;./Dependencies/python38.zip;", nullptr);
+	Py_SetPath(path);
 	Py_NoSiteFlag = 1; // this must be set to 1
 	Py_DontWriteBytecodeFlag = 1;
-	//_PyEval_SetSwitchInterval()
 	
+	// initialize python
 	Py_Initialize();
 	if (!Py_IsInitialized())
 	{
@@ -119,36 +44,20 @@ int main(int argc, char* argv[])
 	PyObject* mmarvel = PyImport_ImportModule("dearpygui");
 
 	// import our custom module to capture stdout/stderr
-	m = PyImport_ImportModule("sandboxout");
+	PyObject* m = PyImport_ImportModule("sandboxout");
 	PySys_SetObject("stdout", m);
 	PySys_SetObject("stderr", m);
 
-	if (editorMode)
-	{
-		PyRun_SimpleString("dearpygui.start_dearpygui_editor()");
-		return 0;
-	}
-
-	if (documentation)
-	{
-		PyRun_SimpleString("dearpygui.start_dearpygui_docs()");
-		return 0;
-	}
-
-	PyObject* pModule = PyImport_ImportModule(AppName.c_str()); // new reference
+	PyObject* pModule = PyImport_ImportModule("Demo"); // new reference
 
 	// check if error occurred
 	if (!PyErr_Occurred() && pModule != nullptr)
 	{
 		Py_XDECREF(pModule);
 		Py_XDECREF(mmarvel);
-		return 0;
 	}
 
 	else
-	{
 		start_dearpygui_error();
-		return 0;
-	}
 	
 }
