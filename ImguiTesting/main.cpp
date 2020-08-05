56d2a42fa563e52c163e52d7c6b2af9d60438e8a
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-08-04 19:27:01 -0500

    added dirent vendor code

--- ImguiTesting/main.cpp
+++ ImguiTesting/main.cpp
@@ -10,6 +10,7 @@
 #endif
 
 #include <implot.h>
+#include <ImGuiFileDialog.h>
 
 int main()
 {
@@ -21,16 +22,36 @@ int main()
     while (window->m_running)
     {
         window->prerender();
-#if defined (_WIN32)
+
         ImGui::ShowDemoWindow();
         ImPlot::ShowDemoWindow();
+
+        igfd::ImGuiFileDialog::Instance()->SetExtentionInfos(".cpp", ImVec4(1, 1, 0, 0.9));
+        igfd::ImGuiFileDialog::Instance()->SetExtentionInfos(".h", ImVec4(0, 1, 0, 0.9));
+        igfd::ImGuiFileDialog::Instance()->SetExtentionInfos(".hpp", ImVec4(0, 0, 1, 0.9));
+        igfd::ImGuiFileDialog::Instance()->SetExtentionInfos(".md", ImVec4(1, 0, 1, 0.9));
+
+        // open Dialog Simple
+        if (ImGui::Button("Open File Dialog"))
+            igfd::ImGuiFileDialog::Instance()->OpenModal("ChooseFileDlgKey", "Choose File", ".cpp,.h,.hpp,.*", ".");
+
+        // display
+        if (igfd::ImGuiFileDialog::Instance()->FileDialog("ChooseFileDlgKey"))
+        {
+            // action if OK
+            if (igfd::ImGuiFileDialog::Instance()->IsOk == true)
+            {
+                std::string filePathName = igfd::ImGuiFileDialog::Instance()->GetFilePathName();
+                std::string filePath = igfd::ImGuiFileDialog::Instance()->GetCurrentPath();
+                // action
+            }
+            // close
+            igfd::ImGuiFileDialog::Instance()->CloseDialog("ChooseFileDlgKey");
+        }
+
         window->postrender();
-#elif defined(__APPLE__)
-#else
-        ImGui::ShowDemoWindow();
-        ImPlot::ShowDemoWindow();
-        window->postrender();
-#endif
+
+
     }
 
 }
