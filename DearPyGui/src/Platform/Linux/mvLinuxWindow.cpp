4b3bf5e47574a4b8ec2a047fa92ea67e69e9093f
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-08-13 18:38:53 -0500

    Pre_CC: [MAC][LINUX]configured imgui windows to be moveable from title only

--- DearPyGui/src/Platform/Linux/mvLinuxWindow.cpp
+++ DearPyGui/src/Platform/Linux/mvLinuxWindow.cpp
@@ -55,6 +55,7 @@ namespace Marvel {
         IMGUI_CHECKVERSION();
         ImGui::CreateContext();
         ImGuiIO& io = ImGui::GetIO(); (void)io;
+        io.ConfigWindowsMoveFromTitleBarOnly = true;
         //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
         //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
 
