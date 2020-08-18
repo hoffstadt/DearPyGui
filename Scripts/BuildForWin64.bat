9685be16a47f17e2abd7de50016f6cdfcf47ab3e
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-08-18 15:04:40 -0500

    Pre_CC: added win64 build script

---
+++ Scripts/BuildForWin64.bat
@@ -0,0 +1,14 @@
+set dir=%~dp0
+call BuildPythonForWindows.bat --ci
+cd ..
+mkdir cmake-build-release
+cd cmake-build-release
+cmake .. -DMVDIST_ONLY=True
+cd ..
+cmake --build cmake-build-release --config Release
+cd Distribution
+call "%dir%..\Dependencies\cpython\PCbuild\amd64\python.exe" "%dir%BuildPythonWheel.py" ../cmake-build-release/DearPyGui/Release/dearpygui.pyd dearpygui.pyd
+call "%dir%..\Dependencies\cpython\PCbuild\amd64\python.exe" -m setup bdist_wheel --plat-name win_amd64 --dist-dir ../dist
+call "%dir%..\Dependencies\cpython\PCbuild\amd64\python.exe" BuildPythonWheel.py ../cmake-build-release/DearPyGui/Release/dearpygui.pyd dearpygui.pyd
+call python -m pip install "%dir%../dist/dearpygui-0.1.0.dev13-cp38-cp38-win_amd64.whl"
+pause
