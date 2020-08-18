2e30294f826522dfeff0542363fc67632de445b4
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-08-18 14:43:43 -0500

    added build script for win32

--- Scripts/PackageWindowsApp.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-rem get current this batch file directory
-set dir=%~dp0
-
-echo Running python package script
-call "%dir%..\Dependencies\cpython\PCbuild\amd64\python.exe" "%dir%..\Scripts\PackageWindowsApp.py"
-echo Complete
