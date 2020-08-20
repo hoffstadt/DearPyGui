f33a0bf6d34379d18d9f3d48d0d3b39fd0a40b80
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-08-20 13:34:25 -0500

    sweep2

--- DearPyGui/src/Core/AppItems/mvSlideItems.h
+++ DearPyGui/src/Core/AppItems/mvSlideItems.h
@@ -8,17 +8,16 @@
 // Widget Index
 //
 //     * mvSliderFloat
-//     * mvSliderFloat2
-//     * mvSliderFloat3
-//     * mvSliderFloat4
 //     * mvSliderInt
-//     * mvSliderInt2
-//     * mvSliderInt3
-//     * mvSliderInt4
+//     * mvSliderFloatMulti
+//     * mvSliderIntMulti
 //
 //-----------------------------------------------------------------------------
 
 namespace Marvel {
+
+	template<typename T>
+	using ImGuiSlideCommand = bool(*)(const char*, T*, T, T, const char*, ImGuiSliderFlags);
 
 	//-----------------------------------------------------------------------------
 	// mvSliderFloat
@@ -80,126 +79,6 @@ namespace Marvel {
 		float       m_max;
 		std::string m_format;
 		bool        m_vertical;
-
-	};
-
-	//-----------------------------------------------------------------------------
-	// mvSliderFloat2
-	//-----------------------------------------------------------------------------
-	class mvSliderFloat2 : public mvFloatItemBase
-	{
-
-	public:
-
-		MV_APPITEM_TYPE(mvAppItemType::SliderFloat2)
-
-		mvSliderFloat2(const std::string& parent, const std::string& name, float default_value[2], float minv = 0.0f,
-				float maxv = 1.0f, std::string  format = "%.3f")
-			: mvFloatItemBase(parent, name, 2, default_value[0], default_value[1]),
-			m_min(minv), m_max(maxv), m_format(std::move(format))
-		{
-		}
-
-		void draw() override
-		{
-			if (ImGui::SliderFloat2(m_label.c_str(), m_value.data(), m_min, m_max, m_format.c_str()))
-			{
-				if (!m_dataSource.empty())
-					mvDataStorage::AddData(m_dataSource, getPyValue());
-
-				mvApp::GetApp()->runCallback(m_callback, m_name);
-
-				// Context Menu
-				if (!getPopup().empty())
-					ImGui::OpenPopup(getPopup().c_str());
-			}
-		}
-
-	private:
-
-		float       m_min;
-		float       m_max;
-		std::string m_format;
-
-	};
-
-	//-----------------------------------------------------------------------------
-	// mvSliderFloat3
-	//-----------------------------------------------------------------------------
-	class mvSliderFloat3 : public mvFloatItemBase
-	{
-
-	public:
-
-		MV_APPITEM_TYPE(mvAppItemType::SliderFloat3)
-
-		mvSliderFloat3(const std::string& parent, const std::string& name, float default_value[3], float minv = 0.0f,
-				float maxv = 1.0f, std::string  format = "%.3f")
-			: mvFloatItemBase(parent, name, 3, default_value[0], default_value[1], default_value[2]),
-			m_min(minv), m_max(maxv), m_format(std::move(format))
-		{
-		}
-
-		void draw() override
-		{
-			if (ImGui::SliderFloat3(m_label.c_str(), m_value.data(), m_min, m_max, m_format.c_str()))
-			{
-				if (!m_dataSource.empty())
-					mvDataStorage::AddData(m_dataSource, getPyValue());
-
-				mvApp::GetApp()->runCallback(m_callback, m_name);
-
-				// Context Menu
-				if (!getPopup().empty())
-					ImGui::OpenPopup(getPopup().c_str());
-			}
-		}
-
-	private:
-
-		float       m_min;
-		float       m_max;
-		std::string m_format;
-
-	};
-
-	//-----------------------------------------------------------------------------
-	// mvSliderFloat4
-	//-----------------------------------------------------------------------------
-	class mvSliderFloat4 : public mvFloatItemBase
-	{
-
-	public:
-
-		MV_APPITEM_TYPE(mvAppItemType::SliderFloat4)
-
-		mvSliderFloat4(const std::string& parent, const std::string& name, float default_value[4], float minv = 0.0f,
-				float maxv = 1.0f, std::string  format = "%.3f")
-			: mvFloatItemBase(parent, name, 4, default_value[0], default_value[1], default_value[2], default_value[3]), 
-			m_min(minv), m_max(maxv), m_format(std::move(format))
-		{
-		}
-
-		void draw() override
-		{
-			if (ImGui::SliderFloat4(m_label.c_str(), m_value.data(), m_min, m_max, m_format.c_str()))
-			{
-				if (!m_dataSource.empty())
-					mvDataStorage::AddData(m_dataSource, getPyValue());
-
-				mvApp::GetApp()->runCallback(m_callback, m_name);
-
-				// Context Menu
-				if (!getPopup().empty())
-					ImGui::OpenPopup(getPopup().c_str());
-			}
-		}
-
-	private:
-
-		float       m_min;
-		float       m_max;
-		std::string m_format;
 
 	};
 
@@ -266,25 +145,25 @@ namespace Marvel {
 	};
 
 	//-----------------------------------------------------------------------------
-	// mvSliderInt2
+	// mvSliderFloatMulti
 	//-----------------------------------------------------------------------------
-	class mvSliderInt2 : public mvIntItemBase
+	template<mvAppItemType AppItemType, int num, ImGuiSlideCommand<float> imguicommand, typename T>
+	class mvSliderFloatMulti : public mvFloatItemBase
 	{
 
 	public:
 
-		MV_APPITEM_TYPE(mvAppItemType::SliderInt2)
+		MV_APPITEM_TYPE(AppItemType)
 
-		mvSliderInt2(const std::string& parent, const std::string& name, int default_value[2], int minv = 0,
-				int maxv = 100, std::string  format = "%d")
-			: mvIntItemBase(parent, name, 2, default_value[0], default_value[1]),
+		mvSliderFloatMulti(const std::string& parent, const std::string& name, T* default_value, T minv, T maxv, std::string  format)
+			: mvFloatItemBase(parent, name, num, default_value[0], default_value[1], default_value[2], default_value[3]),
 			m_min(minv), m_max(maxv), m_format(std::move(format))
 		{
 		}
 
 		void draw() override
 		{
-			if (ImGui::SliderInt2(m_label.c_str(), m_value.data(), m_min, m_max, m_format.c_str()))
+			if (imguicommand(m_label.c_str(), m_value.data(), m_min, m_max, m_format.c_str(), 0))
 			{
 				if (!m_dataSource.empty())
 					mvDataStorage::AddData(m_dataSource, getPyValue());
@@ -299,32 +178,32 @@ namespace Marvel {
 
 	private:
 
-		int         m_min;
-		int         m_max;
+		T       m_min;
+		T       m_max;
 		std::string m_format;
 
 	};
 
 	//-----------------------------------------------------------------------------
-	// mvSliderInt3
+	// mvSliderIntMulti
 	//-----------------------------------------------------------------------------
-	class mvSliderInt3 : public mvIntItemBase
+	template<mvAppItemType AppItemType, int num, ImGuiSlideCommand<int> imguicommand, typename T>
+	class mvSliderIntMulti : public mvIntItemBase
 	{
 
 	public:
 
-		MV_APPITEM_TYPE(mvAppItemType::SliderInt3)
+		MV_APPITEM_TYPE(AppItemType)
 
-		mvSliderInt3(const std::string& parent, const std::string& name, int default_value[3], int minv = 0,
-				int maxv = 100, std::string  format = "%d")
-			: mvIntItemBase(parent, name, 3, default_value[0], default_value[1], default_value[2]),
+		mvSliderIntMulti(const std::string& parent, const std::string& name, T* default_value, T minv, T maxv, std::string  format)
+			: mvIntItemBase(parent, name, num, default_value[0], default_value[1], default_value[2], default_value[3]),
 			m_min(minv), m_max(maxv), m_format(std::move(format))
 		{
 		}
 
 		void draw() override
 		{
-			if (ImGui::SliderInt3(m_label.c_str(), m_value.data(), m_min, m_max, m_format.c_str()))
+			if (imguicommand(m_label.c_str(), m_value.data(), m_min, m_max, m_format.c_str(), 0))
 			{
 				if (!m_dataSource.empty())
 					mvDataStorage::AddData(m_dataSource, getPyValue());
@@ -339,48 +218,8 @@ namespace Marvel {
 
 	private:
 
-		int         m_min;
-		int         m_max;
-		std::string m_format;
-
-	};
-
-	//-----------------------------------------------------------------------------
-	// mvSliderInt4
-	//-----------------------------------------------------------------------------
-	class mvSliderInt4 : public mvIntItemBase
-	{
-
-	public:
-
-		MV_APPITEM_TYPE(mvAppItemType::SliderInt4)
-
-		mvSliderInt4(const std::string& parent, const std::string& name, int default_value[4], int minv = 0,
-				int maxv = 100, std::string  format = "%d")
-			: mvIntItemBase(parent, name, 4, default_value[0], default_value[1], default_value[2], default_value[3]),
-			m_min(minv), m_max(maxv), m_format(std::move(format))
-		{
-		}
-
-		void draw() override
-		{
-			if (ImGui::SliderInt4(m_label.c_str(), m_value.data(), m_min, m_max, m_format.c_str()))
-			{
-				if (!m_dataSource.empty())
-					mvDataStorage::AddData(m_dataSource, getPyValue());
-
-				mvApp::GetApp()->runCallback(m_callback, m_name);
-
-				// Context Menu
-				if (!getPopup().empty())
-					ImGui::OpenPopup(getPopup().c_str());
-			}
-		}
-
-	private:
-
-		int         m_min;
-		int         m_max;
+		T       m_min;
+		T       m_max;
 		std::string m_format;
 
 	};
