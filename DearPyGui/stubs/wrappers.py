58ea59a37968ed41c9502045eac75a13696920a7
Alex Davies <traverse.da@gmail.com>
2020-09-06 11:24:17 -0300

    Automatically generated wrappers

--- DearPyGui/stubs/wrappers.py
+++ DearPyGui/stubs/wrappers.py
@@ -1,75 +1,23 @@
 from contextlib import contextmanager
-from dearpygui.dearpygui import *
+from functools import wraps
+import dearpygui.dearpygui as gui
 
-@contextmanager
-def window(name: str, width: int = -1, height: int = -1, start_x: int = 200, start_y: int = 200, 
-           autosize: bool = False, resizable: bool = True, title_bar: bool = True, 
-           movable: bool = True, hide: bool = False, on_close: str = ""):
-    try: 
-        yield add_window(name, width, height, start_x=start_x, start_y=start_y, autosize=autosize, 
-                         resizable=resizable, title_bar=title_bar, movable=movable, hide=hide,
-                         on_close=on_close)
-    finally: end()
+def wrap_container(container):
+    @contextmanager
+    @wraps(container)
+    def container_context(*args,**kwargs):
+        try: yield container(*args,**kwargs)
+        finally: end()
+    return container_context
 
-@contextmanager
-def menu_bar(name: str, parent: str = "", before: str = ""):
-    try: yield add_menu_bar(name, parent=parent, before=before)
-    finally: end()
-
-@contextmanager
-def menu(name: str, tip: str = "", parent: str = "", before: str = ""):
-    try: yield add_menu(name, tip=tip, parent=parent, before=before)
-    finally: end()
-
-@contextmanager
-def child(name: str, tip: str = "", parent: str = "", before: str = "", width: int = 0, height: int = 0, border: bool = True):
-    try: 
-        yield add_child(name, tip=tip, parent=parent, before=before, width=width, 
-                        height=height, border=border)
-    finally: end()
-
-@contextmanager
-def collapsing_header(name: str, default_open: bool = False, closable: bool = False, tip: str = "", parent: str = "", before: str = ""):
-    try:
-        yield add_collapsing_header(name, default_open=default_open, closable=closable, tip=tip, parent=parent, before=before)
-    finally: end()
-
-@contextmanager
-def group(name: str, tip: str = "", parent: str = "", before: str = "", width: int = 0, hide: bool = False, 
-			  horizontal: bool = False, horizontal_spacing: float = -1.0):
-    try:
-        yield add_group(name, tip=tip, parent=parent, before=before, width=width, hide=hide, horizontal=horizontal,
-                        horizontal_spacing=horizontal_spacing)
-    finally: end()
-
-@contextmanager
-def tab_bar(name: str, reorderable: bool = False, callback: str = "", parent: str = "", before: str = "", data_source: str = ""):
-    try:
-        yield add_tab_bar(name, reorderable=reorderable, callback=callback, parent=parent, before=before, data_source=data_source)
-    finally: end()
-
-@contextmanager
-def tab(name: str, closable: bool = False, tip: str = "", parent: str = "", before: str = ""):
-    try:
-        yield add_tab(name, closable=closable, tip=tip, parent=parent, before=before)
-    finally: end()
-
-@contextmanager
-def tree_node(name: str, default_open: bool = False, tip: str = "", parent: str = "", before: str = ""):
-    try:
-        yield add_tree_node(name, default_open=default_open, tip=tip, parent=parent, before=before)
-    finally: end()
-
-@contextmanager
-def tooltip(tipparent: str, name: str, parent: str = "", before: str = ""):
-    try:
-        yield add_tooltip(tipparent, name, parent=parent, before=before)
-    finally: end()
-
-@contextmanager
-def popup(popupparent: str, name: str, mousebutton: int = 1, modal: bool = False, parent: str = "", 
-          before: str = "", width: int = 0, height: int = 0):
-    try:
-        yield add_popup(popupparent, name, mousebutton=mousebutton, modal=modal, parent=parent,
-                        before=before, width=width, height=height)
-    finally: end()
+window = wrap_container(gui.add_window)
+menu_bar = wrap_container(gui.add_menu_bar)
+menu = wrap_container(gui.add_menu)
+child = wrap_container(gui.add_child)
+collapsing_header = wrap_container(gui.add_collapsing_header)
+group = wrap_container(gui.add_group)
+tab_bar = wrap_container(gui.add_tab_bar)
+tab = wrap_container(gui.add_tab)
+tree_node = wrap_container(gui.add_tree_node)
+tooltip = wrap_container(gui.add_tooltip)
+popup = wrap_container(gui.add_popup)
