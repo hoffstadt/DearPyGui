a2bb1f513a0ea13db882c42a00523ef94c94aca8
Preston Cothren <40273423+Pcothren@users.noreply.github.com>
2020-09-09 08:02:13 -0500

    Pre_CC: Issue196 (#197)
    * [issue 196 Fixed] and added login example
    
    * fixed typo
    
    * Revert "fixed typo"
    
    This reverts commit 476bd7c032818c251ad6fe1bea67c4422b7dd5c8.
    
    * fixed typo

---
+++ Examples/login_example.py
@@ -0,0 +1,44 @@
+from dearpygui.dearpygui import *
+from dearpygui.wrappers import *
+
+def recursively_show(container):
+    for item in get_item_children(container):
+        if get_item_children(item):
+            show_item(item)
+            recursively_show(item)
+        else:
+            show_item(item)
+
+def try_login(sender, data):
+    print(sender)
+    print(data)
+    if get_value('Password') == 'password':
+        delete_item('Login Window')
+        recursively_show('MainWindow')
+    else:
+        show_item('Incorrect Password.')
+
+def position_login_window(sender, data):
+    main_width = get_item_width('MainWindow')
+    main_height = get_item_height('MainWindow')
+    login_width = get_item_width('Login Window')
+    login_height = get_item_height('Login Window')
+    set_window_pos('Login Window', (main_width/2 - login_width/2), (main_height/2 - login_height/2))
+
+with window('Login Window', title_bar=False, movable=False, autosize=True, resizable=False):
+    add_input_text('Password', hint='hover me for password', password=True, tip='the password is "password"')
+    add_button('Login', callback=try_login)
+    add_text('Incorrect Password.', color=[255, 0, 0], parent='Login Window')
+    hide_item('Incorrect Password.')
+    set_render_callback(position_login_window, 'Login Window')
+
+with menu_bar('menu bar'):
+    with menu('menu 1'):
+        add_menu_item('menu item')
+    with menu('menu 2'):
+        pass
+
+add_text('Congrats!, you may now use the app')
+add_button('Button 1')
+hide_item('MainWindow', children_only=True)
+start_dearpygui()
