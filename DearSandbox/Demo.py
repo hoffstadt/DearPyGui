c7c5ac3f1e50ea4d1fdbdf83da0980389eb0e277
Preston Cothren <prestoncothren@yahoo.com>
2020-09-28 19:06:26 -0500

    done

--- DearSandbox/Demo.py
+++ DearSandbox/Demo.py
@@ -1,31 +1,42 @@
 from dearpygui.core import *
 from dearpygui.simple import *
+from dearpygui.demo import *
+from math import sin, cos
+import random
 
-def update_drawing(sender, data):
-    set_drawing_origin("drawing##widget", get_value("X Origin"), get_value("Y Origin"))
-    set_drawing_scale("drawing##widget", get_value("X Scale "), get_value("Y Scale"))
+########################################################################################################################
+# Settings and Data Storage
+########################################################################################################################
+set_log_level(0)
 
-with group("Drawing Controls Group"):
-    add_slider_float("X Origin", vertical=True, min_value=-100, max_value=100, default_value=0, callback=update_drawing)
-    add_same_line(spacing=20)
-    add_slider_float("Y Origin", vertical=True, min_value=-100, max_value=100, default_value=0, callback=update_drawing)
-    add_slider_float("X Scale ", vertical=True, max_value=10, default_value=1, callback=update_drawing)
-    add_same_line(spacing=20)
-    add_slider_float("Y Scale", vertical=True, max_value=10, default_value=1, callback=update_drawing)
+set_main_window_title("DearPyGui Demo")
+set_main_window_size(1000, 800)
+set_main_window_pos(0, 0)
+add_additional_font("C:/dev/DearPyGui/Resources/NotoSerifCJKjp-Medium.otf", 20)
 
-add_same_line(spacing=20)
-add_drawing("drawing##widget", width=800, height=500)
-draw_rectangle("drawing##widget", [0, 500], [800, 0], [255, 0, 0, 255], fill=[0, 0, 25, 255], rounding=12,
-               thickness=1.0)
-draw_line("drawing##widget", [10, 10], [100, 100], [255, 0, 0, 255], 1)
-draw_triangle("drawing##widget", [300, 500], [200, 200], [500, 200], [255, 255, 0, 255], thickness=3.0)
-draw_quad("drawing##widget", [50, 50], [150, 50], [150, 150], [50, 150], [255, 255, 0, 255], thickness=3.0)
-draw_text("drawing##widget", [50, 300], "Some Text", color=[255, 255, 0, 255], size=15)
-draw_text("drawing##widget", [0, 0], "Origin", color=[255, 255, 0, 255], size=15)
-draw_circle("drawing##widget", [400, 250], 50, [255, 255, 0, 255])
-draw_polyline("drawing##widget", [[300, 500], [200, 200], [500, 700]], [255, 255, 0, 255])
-draw_polygon("drawing##widget", [[363, 471], [100, 498], [50, 220]], [255, 125, 0, 255])
-draw_bezier_curve("drawing##widget", [50, 200], [150, 250], [300, 150], [600, 250], [255, 255, 0, 255], thickness=2.0)
-draw_arrow("drawing##widget", [50, 70], [100, 65], [0, 200, 255], 1, 10)
+show_demo()
+
+with window("Asyncronous##dialog", show=False):
+    add_data('threadNumber', 0)
+    def LongCallback2(sender, data):
+        sleep(5)
+        return data
+
+    def ReturnFromLongProcess(sender, data):
+        log_info("Completed process number: " + str(data))
+        add_data('threadNumber', get_data('threadNumber')-1)
+
+    def LongAsyncronousCallback(sender, data):
+        current_number = get_data('threadNumber')
+        run_async_function(LongCallback2, current_number, return_handler=ReturnFromLongProcess)
+        log_info("Start process number: " + str(current_number))
+        add_data('threadNumber', current_number+1)
+
+    def LongCallback(sender, data):
+        sleep(5)
+        log_info("Done with long process")
+
+    add_button("Start Long Process", callback=LongCallback)
+    add_button("Start Long Asyncronous Process", callback=LongAsyncronousCallback)
 
 start_dearpygui()
