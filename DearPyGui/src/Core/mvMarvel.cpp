76bcb99bf340a1d2ec283f0afa9647fdfa3640e6
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-11-09 20:26:42 -0600

    added candle series

--- DearPyGui/src/Core/mvMarvel.cpp
+++ DearPyGui/src/Core/mvMarvel.cpp
@@ -789,6 +789,7 @@ namespace Marvel {
 		ADD_PYTHON_FUNCTION(add_error_series)
 		ADD_PYTHON_FUNCTION(add_image_series)
 		ADD_PYTHON_FUNCTION(add_stair_series)
+		ADD_PYTHON_FUNCTION(add_candle_series)
 		ADD_PYTHON_FUNCTION(delete_series)
 		ADD_PYTHON_FUNCTION(add_heat_series)
 		ADD_PYTHON_FUNCTION(add_text_point)
