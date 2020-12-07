47ba187d824c089a47d6c03ec9b0e80ed22a75f5
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-12-07 08:29:59 -0600

    Pre_CC: (release) fixed window width issue

--- DearPyGui/src/core/AppItems/mvAppItemState.cpp
+++ DearPyGui/src/core/AppItems/mvAppItemState.cpp
@@ -34,11 +34,9 @@ namespace Marvel {
         m_rectMax = { ImGui::GetItemRectMax().x, ImGui::GetItemRectMax().y };
         m_rectSize = { ImGui::GetItemRectSize().x, ImGui::GetItemRectSize().y };
 
-        if (!m_parent->getDescription().container)
-        {
-                m_parent->m_actualWidth = m_rectSize.x;
-                m_parent->m_actualHeight = m_rectSize.y;
-        }
+        m_parent->m_actualWidth = m_rectSize.x;
+        m_parent->m_actualHeight = m_rectSize.y;
+
     }
 
 }
