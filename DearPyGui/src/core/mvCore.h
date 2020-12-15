e9c86a21ec3aa75203ddd84df1e75c8dcc7949c4
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-12-15 11:48:06 -0600

    [skip ci] added mvRef and mvScope

--- DearPyGui/src/core/mvCore.h
+++ DearPyGui/src/core/mvCore.h
@@ -9,8 +9,25 @@
 
 #include <imgui.h>
 #include <utility>
+#include <memory>
 
 namespace Marvel {
+
+	template<typename T>
+	using mvOwnedPtr = std::unique_ptr<T>;
+	template<typename T, typename ... Args>
+	constexpr mvOwnedPtr<T> CreateOwnedPtr(Args&& ... args)
+	{
+		return std::make_unique<T>(std::forward<Args>(args)...);
+	}
+
+	template<typename T>
+	using mvRef = std::shared_ptr<T>;
+	template<typename T, typename ... Args>
+	constexpr mvRef<T> CreateRef(Args&& ... args)
+	{
+		return std::make_shared<T>(std::forward<Args>(args)...);
+	}
 
 	//-----------------------------------------------------------------------------
 	// mvVec2
