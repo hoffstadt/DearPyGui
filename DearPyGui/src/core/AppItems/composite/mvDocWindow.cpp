e478bab46e90b8e0077bdbff56e826be52799318
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2020-12-18 00:50:18 -0600

    [skip ci] small fixes

--- DearPyGui/src/core/AppItems/composite/mvDocWindow.cpp
+++ DearPyGui/src/core/AppItems/composite/mvDocWindow.cpp
@@ -163,11 +163,10 @@ namespace Marvel {
 
 
 				ColorText("ABOUT THIS GUIDE:");
-				ImGui::BulletText("Sections below cover the basic aspects of DearPyGui Core.");
+				ImGui::BulletText("Sections below cover the basic aspects of Dear PyGui Core.");
 				ImGui::BulletText("This document can be found by calling the \"show_documentation\" command.");
 				ImGui::BulletText("Additional information on specific commands can be found in the commands tab.");
 				ImGui::BulletText("The complete documentation can be found at: https://hoffstadt.github.io/DearPyGui/");
-
 
 				if (ImGui::CollapsingHeader("Logging and Console Output"))
 				{
@@ -207,7 +206,7 @@ namespace Marvel {
 
 					ImGui::Separator();
 					CodeColorText("ERROR"); ImGui::NextColumn();
-					ColorText("ERROR"); ImGui::NextColumn();
+					ColorText("mvERROR"); ImGui::NextColumn();
 					CodeColorText("log_error('some message')"); ImGui::NextColumn();
 
 					ImGui::Separator();
@@ -332,7 +331,7 @@ namespace Marvel {
 					ImGui::BulletText("METHOD 1 (RECOMMENDED):");
 					ImGui::Indent();
 					ImGui::Text("You simply call the add_* commands, while creating a nested hierarchy by");
-					ImGui::Text("placing widgets between the add_* and end_* of container widgets.");
+					ImGui::Text("placing widgets between the add_* and end of container widgets.");
 					ImGui::Text("With this method, widgets automatically know which containers they");
 					ImGui::Text("belong to. Example:");
 					ImGui::Spacing();
@@ -375,18 +374,16 @@ namespace Marvel {
 				if (ImGui::CollapsingHeader("Value Storage"))
 				{
 					ColorText("BASICS");
-					ImGui::BulletText("There are 3 use cases for the data storage system:");
+					ImGui::BulletText("There are 2 use cases for the data storage system:");
 					ImGui::Indent();
 					ImGui::BulletText("1. For widgets to use the same underlying data (i.e. both modify the same data)");
 					ImGui::BulletText("2. To store data for use at a later time or in a different callback.");
-					ImGui::BulletText("3. A combination of 1 and 2.");
 					ImGui::Unindent();
 					ImGui::Separator();
 
 					ColorText("USE CASE 1");
 					ImGui::BulletText("To share the underlying data of multiple widgets, just use the 'source' keyword when adding the widget.");
 					ImGui::BulletText("Ensure the underlying data structures match or it will not work.");
-					ImGui::BulletText("Some widgets have a 'secondary_data_source' (i.e. listbox, which allows modification of the listed items)");
 					ImGui::Separator();
 
 					ColorText("USE CASE 2");
@@ -398,16 +395,13 @@ namespace Marvel {
 					ImGui::Unindent();
 					ImGui::Separator();
 
-					ColorText("USE CASE 3");
-					ImGui::BulletText("With this use case, you store data with 'add_data' and use that data with the 'ource' keyword.");
-
 				}
 
 				if (ImGui::CollapsingHeader("Callbacks"))
 				{
 					ColorText("BASICS:");
 					ImGui::BulletText("Callbacks are functions that are ran when some event occurs.");
-					ImGui::BulletText("There are currently 3 'types' of callbacks used in DearPyGui:");
+					ImGui::BulletText("There are currently 3 'types' of callbacks used in Dear PyGui:");
 					ImGui::Indent();
 					CodeColorText("Render  - ran every frame");
 					CodeColorText("Inputs  - ran when mouse/keyboard events occurr");
@@ -416,7 +410,7 @@ namespace Marvel {
 					ImGui::BulletText("Notes:");
 					ImGui::Indent();
 					ImGui::BulletText("The 'set_*_callback' commands take a string with the name of the callback to use.");
-					ImGui::BulletText("Always be ran on the same thread as the GUI.");
+					ImGui::BulletText("Always ran on the same thread as the GUI.");
 					ImGui::BulletText("Should NOT be computationally expensive (because of the note above),");
 					ImGui::Text("otherwise you may see a drop in frame rate.");
 					ImGui::Unindent();
@@ -439,7 +433,6 @@ namespace Marvel {
 					ColorText("INPUT CALLBACKS:");
 					ImGui::BulletText("Useful for capturing key strokes, making iteractive drawings, etc.");
 					ImGui::BulletText("Commands take the form of 'set_*_callback(...)'");
-					ImGui::BulletText("The optional 'handler' keyword will set which window triggers the callback.");
 					ImGui::BulletText("Most should take the following form:");
 					ImGui::Indent();
 					CodeColorText("def callbackname(sender, data):");
@@ -458,7 +451,6 @@ namespace Marvel {
 					CodeColorText("    ...");
 					ImGui::Unindent();
 					ImGui::BulletText("The 'sender' parameter is the name of widget the callback was sent from.");
-					ImGui::BulletText("The data parameter depends on the actual callback. Check the documentation.");
 
 				}
 
@@ -468,7 +460,7 @@ namespace Marvel {
 					ColorText("BASICS:");
 					ImGui::BulletText("If a callback needs to perform time consuming calculations, functions can be");
 					ImGui::Text("    ran asyncronously (on seperate threads).");
-					ImGui::BulletText("Currently, the async function CAN NOT call Marvel API commands.");
+					ImGui::BulletText("Currently, the async function can only call some Dear PyGui commands.");
 
 					ImGui::BulletText("The following command is used for asyncronous functions:");
 					ImGui::Indent();
@@ -527,17 +519,17 @@ namespace Marvel {
 				if (ImGui::CollapsingHeader("Drawings"))
 				{
 					ColorText("BASICS:");
-					ImGui::BulletText("The drawing API is a low-level drawing API useful as a canvas or custom widget.");
+					ImGui::BulletText("The drawing API is a low-level drawing API useful for simple graphics.");
 					ImGui::BulletText("Drawings are added using the 'add_drawing(...)' command.");
-					ImGui::BulletText("All drawing commands' first argument is the name of the drawing you are refering to.");
-					ImGui::BulletText("The drawings scale and coordinate origin can be set through keywords");
+					ImGui::BulletText("All drawing commands' first argument can either refer to a drawing widget,");
+					ImGui::Text("    window, '##FOREGROUND', or '##BACKGROUND'.");
 					ImGui::Separator();
 
 					ColorText("TAGS:");
 					ImGui::BulletText("To efficiently update a drawing, you should use the tag system (as opposed to clearing and redrawing)");
 					ImGui::BulletText("All draw commands of the form 'draw_*' accept an optional tag argument.");
 					ImGui::BulletText("The tag argument is a string that acts as an indentifier for the draw item.");
-					ImGui::BulletText("On subsequent calls to the same draw command with the given tag, the item will be updated.");
+					ImGui::BulletText("You can update previously submitted draw commands using 'modify_draw_command'.");
 				}
 
 				ImGui::EndTabItem();
@@ -705,7 +697,6 @@ namespace Marvel {
 
 		ImGui::End();
 	}
-
 
 	PyObject* add_doc_window(PyObject* self, PyObject* args, PyObject* kwargs)
 	{
