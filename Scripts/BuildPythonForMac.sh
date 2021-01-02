950cf9091d1ade5ed37d701acb8ce1c805fff3f6
Yan <mindv0rtex@users.noreply.github.com>
2021-01-01 23:18:25 -0500

    Set Python path for OSX to avoid clashing with other pre-installed Python interpreters. (#427)

--- Scripts/BuildPythonForMac.sh
+++ Scripts/BuildPythonForMac.sh
@@ -1,5 +1,6 @@
 cd ../Dependencies/cpython
 mkdir debug
 cd debug
-../configure --with-pydebug --enable-shared
+../configure --with-pydebug --enable-shared --prefix=$(pwd) LDFLAGS=-Wl,-rpath,$(pwd)
 make
+make install
