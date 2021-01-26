a22a43c6d61a3bee58fc0275ec88b90d3f39fac8
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2021-01-26 08:00:08 -0600

    (release) v0.6.146

--- DearPyGui/src/core/AppItems/basic/mvImageButton.cpp
+++ DearPyGui/src/core/AppItems/basic/mvImageButton.cpp
@@ -72,11 +72,14 @@ namespace Marvel {
 			mvTexture* texture = mvApp::GetApp()->getTextureStorage().getTexture(m_value);
 			if (texture == nullptr)
 			{
-				PyErr_Format(PyExc_Exception,
-					"Image %s could not be found for add_image. Check the path to the image "
-					"you provided.", m_value.c_str());
-				PyErr_Print();
-				m_value = "";
+				mvApp::GetApp()->getCallbackRegistry().submitCallback([&]()
+					{
+						PyErr_Format(PyExc_Exception,
+							"Image %s could not be found for add_image_button. Check the path to the image "
+							"you provided.", m_value.c_str());
+						PyErr_Print();
+						m_value = "";
+					});
 				return;
 			}
 			if (m_width == 0) m_width = (int)((float)texture->width * (m_uv_max.x - m_uv_min.x));
