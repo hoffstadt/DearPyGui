257b1148d4884801e9ed3f5d223bb8dcc585cc1f
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2021-02-12 09:17:16 -0600

    [skip ci] fixed linux build script

--- Scripts/BuildLocalWheelMacIntel.sh
+++ Scripts/BuildLocalWheelMacIntel.sh
@@ -1,10 +1,12 @@
+brew install openssl xz gdbm
 cd ../Dependencies/cpython
-mkdir release
-cd release
-../configure --enable-shared --prefix=$(pwd) LDFLAGS=-Wl,-rpath,$(pwd)
+mkdir debug
+cd debug
+../configure --enable-shared --prefix=$(pwd) LDFLAGS=-Wl,-rpath,$(pwd) --with-openssl=$(brew --prefix openssl)
 make
 make install
 
+cd ../../..
 mkdir cmake-build-release
 cd cmake-build-release
 cmake .. -DMVDIST_ONLY=True -DMVPY_VERSION=0 -DMVDPG_VERSION=local_build
@@ -12,7 +14,7 @@ cd ..
 cmake --build cmake-build-release --config Release
 
 cd Distribution
-"../Dependencies/cpython/debug/python.exe" BuildPythonWheel.py ../cmake-build-release/DearPyGui/Release/core.so 0
+"../Dependencies/cpython/debug/python.exe" BuildPythonWheel.py ../cmake-build-release/DearPyGui/core.so 0
 "../Dependencies/cpython/debug/python.exe" -m ensurepip
 "../Dependencies/cpython/debug/python.exe" -m pip install --upgrade pip
 "../Dependencies/cpython/debug/python.exe" -m pip install twine --upgrade
