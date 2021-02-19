25db149095ae2f5554d17eed1af6a4aef9091a3c
Preston Cothren <prestoncothren@yahoo.com>
2021-02-18 19:01:28 -0600

    [skip ci] Style (#602)
    * complete style item constants
    
    * removed release cmake list text file
    
    * readd cmake with correct name

--- DearPyGui/src/core/AppItems/containers/mvTreeNode.h
+++ DearPyGui/src/core/AppItems/containers/mvTreeNode.h
@@ -19,15 +19,25 @@ namespace Marvel {
 	{
 		MV_APPITEM_TYPE(mvAppItemType::TreeNode, mvTreeNode, "add_tree_node")
 
-		MV_CREATE_THEME_CONSTANT(mvAppItemType::TreeNode, mvThemeCol_TreeNode_Text		,  0L, 0L);
-		MV_CREATE_THEME_CONSTANT(mvAppItemType::TreeNode, mvThemeCol_TreeNode_BgHovered	, 25L, 0L);
-		MV_CREATE_THEME_CONSTANT(mvAppItemType::TreeNode, mvThemeCol_TreeNode_BgActive	, 26L, 0L);
+		MV_CREATE_THEME_CONSTANT(mvAppItemType::TreeNode, mvThemeCol_TreeNode_Text				,  0L, 0L);
+		MV_CREATE_THEME_CONSTANT(mvAppItemType::TreeNode, mvThemeCol_TreeNode_BgHovered			, 25L, 0L);
+		MV_CREATE_THEME_CONSTANT(mvAppItemType::TreeNode, mvThemeCol_TreeNode_BgActive			, 26L, 0L);
+		MV_CREATE_THEME_CONSTANT(mvAppItemType::TreeNode, mvThemeStyle_TreeNode_FramePaddingX	, 10L, 0L);
+		MV_CREATE_THEME_CONSTANT(mvAppItemType::TreeNode, mvThemeStyle_TreeNode_FramePaddingY	, 10L, 1L);
+		MV_CREATE_THEME_CONSTANT(mvAppItemType::TreeNode, mvThemeStyle_TreeNode_IndentSpacing	, 15L, 0L);
+
 
 		MV_START_COLOR_CONSTANTS
 			MV_CREATE_CONSTANT_PAIR(mvThemeCol_TreeNode_Text),
 			MV_CREATE_CONSTANT_PAIR(mvThemeCol_TreeNode_BgHovered),
 			MV_CREATE_CONSTANT_PAIR(mvThemeCol_TreeNode_BgActive),
 		MV_END_COLOR_CONSTANTS
+
+		MV_START_STYLE_CONSTANTS
+			MV_CREATE_CONSTANT_TUPLE(mvThemeStyle_TreeNode_FramePaddingX, 0, 20),
+			MV_CREATE_CONSTANT_TUPLE(mvThemeStyle_TreeNode_FramePaddingY, 0, 20),
+			MV_CREATE_CONSTANT_TUPLE(mvThemeStyle_TreeNode_IndentSpacing, 0, 30),
+		MV_END_STYLE_CONSTANTS
 
 	public:
 
