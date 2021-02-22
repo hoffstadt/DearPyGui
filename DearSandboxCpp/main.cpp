8ced4a40b2def65bf0d2a13738f1b0762337d241
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2021-02-22 09:49:15 -0600

    Pre_CC: added cpp interface to combo widget

--- DearSandboxCpp/main.cpp
+++ DearSandboxCpp/main.cpp
@@ -14,6 +14,8 @@ int main(int argc, char* argv[])
 
 		add_checkbox("Click me");
 
+		add_combo("Combo", { std::string("Item 1"), std::string("Item 2"), std::string("Item 3") }, nullptr);
+
 		add_color_button("Color button", mvColor(255, 100, 0, 255), []() {std::cout << "Color Button Pressed" << std::endl; });
 		add_color_edit3("Color edit 3");
 		add_color_picker4("Color Picker 4");
