e744dc2e6d7d42205ca11ab7e1666281732520ec
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2021-02-26 09:36:37 -0600

    [skip ci] corrected sandbox

--- DearSandbox/sandbox.py
+++ DearSandbox/sandbox.py
@@ -1,39 +1,23 @@
 from dearpygui.core import *
 from dearpygui.simple import *
+from dearpygui.demo import *
+from math import sin, cos
+import random
+import time
 
+set_log_level(0)
+#enable_docking(shift_only=False, dock_space=True)
 
-def log_values(sender):
-    caller_value = get_value(sender)
-    log_info(message=f"{sender}: {caller_value}", logger="Example Logger")
+set_main_window_title("DearPyGui Demo")
+set_main_window_size(1000, 800)
+set_main_window_pos(0, 0)
+add_additional_font("../../Resources/NotoSerifCJKjp-Medium.otf", 20)
 
+# char remaps
+#add_character_remap(0x0041, 0x00A2)
+#add_character_remap(0x0061, 0x00AB)
 
-def enable_widgets():
-    log_warning("Enabling all widgets", logger="Example Logger")
-    configure_item("slider_int_input_allow_clamped", enabled=True)
-    configure_item("slider_int_no_input", enabled=True)
-    configure_item("slider_int_no_input_start_disabled_clamped", enabled=True)
+show_demo()
+show_logger()
 
-
-def disable_widgets():
-    log_warning("Disabling all widgets", logger="Example Logger")
-    configure_item("slider_int_input_allow_clamped", enabled=False)
-    configure_item("slider_int_no_input", enabled=False)
-    configure_item("slider_int_no_input_start_disabled_clamped", enabled=False)
-
-
-with window("Example Window"):
-    add_button("Enable all inputs", callback=enable_widgets)
-    add_same_line()
-    add_button("Disable all inputs", callback=disable_widgets)
-    add_spacing()
-    add_separator()
-    add_spacing()
-    add_text("All sliders below have a minimum clamped value of 0 and a max value of 15.")
-    add_slider_int("slider_int_input_allow_clamped", min_value=0, max_value=15, clamped=True, callback=log_values)
-    add_slider_int("slider_int_no_input", min_value=0, max_value=15, callback=log_values, no_input=True, clamped=True)
-    add_slider_int("slider_int_no_input_start_disabled_clamped", min_value=0, max_value=15, callback=log_values,
-                   no_input=True, enabled=False, clamped=True)
-    add_logger(name="Example Logger", autosize_x=True, autosize_y=True)
-
-
-start_dearpygui(primary_window="Example Window")
+start_dearpygui()
