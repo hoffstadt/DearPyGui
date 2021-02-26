3f335c516cc387d343b4b4b338097d6352151d58
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2021-02-26 08:56:57 -0600

    [skip ci] fixed window config not returning specified label #627

--- DearPyGui/src/core/AppItems/containers/mvWindowAppItem.cpp
+++ DearPyGui/src/core/AppItems/containers/mvWindowAppItem.cpp
@@ -108,7 +108,8 @@ namespace Marvel {
 
 	void mvWindowAppItem::setLabel(const std::string& value)
 	{
-		m_label = value;
+		m_specifiedLabel = value;
+		m_label = value + "###" + m_name;
 		m_dirty_pos = true;
 		m_dirty_size = true;
 	}
