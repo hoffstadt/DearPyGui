4254ccc7a9a7fffc124b9464a3152cd41ffd9925
Preston Cothren <prestoncothren@yahoo.com>
2021-03-02 22:43:20 -0600

    replace sandbox.py

---
+++ DearSandbox/sandbox.py
@@ -0,0 +1,24 @@
+from dearpygui.core import *
+from dearpygui.simple import *
+from dearpygui.demo import *
+from math import sin, cos
+import random
+import time
+
+set_log_level(0)
+#enable_docking(shift_only=False, dock_space=True)
+
+set_main_window_title("DearPyGui Demo")
+set_main_window_size(1000, 800)
+set_main_window_pos(0, 0)
+add_additional_font("../../Resources/NotoSerifCJKjp-Medium.otf", 20)
+
+# char remaps
+#add_character_remap(0x0041, 0x00A2)
+#add_character_remap(0x0061, 0x00AB)
+
+show_demo()
+show_logger()
+show_metrics()
+
+start_dearpygui()
