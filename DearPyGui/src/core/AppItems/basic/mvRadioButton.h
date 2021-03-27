76020de6cce39672d66587e92e82c9b980744142
preston cothren <prestoncothren@yahoo.com>
2021-03-27 13:41:29 -0500

    expanding diable color expanded disable colors to all currently capable widgets

--- DearPyGui/src/core/AppItems/basic/mvRadioButton.h
+++ DearPyGui/src/core/AppItems/basic/mvRadioButton.h
@@ -32,12 +32,12 @@ namespace Marvel {
 		MV_CREATE_THEME_CONSTANT(mvThemeStyle_RadioButton_InnerSpacingY, 14L, 1L);
 
 		MV_START_COLOR_CONSTANTS
-			MV_CREATE_CONSTANT_PAIR(mvThemeCol_RadioButton_Text,			mvColor(255, 255, 255, 255)),
-			MV_CREATE_CONSTANT_PAIR(mvThemeCol_RadioButton_Bg,				mvColor( 41,  74, 122, 138)),
-			MV_CREATE_CONSTANT_PAIR(mvThemeCol_RadioButton_BgHovered,		mvColor( 66, 150, 250, 102)),
-			MV_CREATE_CONSTANT_PAIR(mvThemeCol_RadioButton_BgActive,		mvColor( 66, 150, 250, 171)),
-			MV_CREATE_CONSTANT_PAIR(mvThemeCol_RadioButton_Border,			mvColor(110, 110, 128, 128)),
-			MV_CREATE_CONSTANT_PAIR(mvThemeCol_RadioButton_BorderShadow,	mvColor(  0,   0,   0,   0)),
+			MV_CREATE_CONSTANT_TUPLE(mvThemeCol_RadioButton_Text,			mvColor(255, 255, 255, 255), mvColor(128, 128, 128, 63)),
+			MV_CREATE_CONSTANT_TUPLE(mvThemeCol_RadioButton_Bg,				mvColor( 41,  74, 122, 138), mvColor(128, 128, 128, 63)),
+			MV_CREATE_CONSTANT_TUPLE(mvThemeCol_RadioButton_BgHovered,		mvColor( 66, 150, 250, 102), mvColor(128, 128, 128, 63)),
+			MV_CREATE_CONSTANT_TUPLE(mvThemeCol_RadioButton_BgActive,		mvColor( 66, 150, 250, 171), mvColor(128, 128, 128, 63)),
+			MV_CREATE_CONSTANT_TUPLE(mvThemeCol_RadioButton_Border,			mvColor(110, 110, 128, 128), mvColor(128, 128, 128, 63)),
+			MV_CREATE_CONSTANT_TUPLE(mvThemeCol_RadioButton_BorderShadow,	mvColor(  0,   0,   0,   0), mvColor(128, 128, 128, 63)),
 		MV_END_COLOR_CONSTANTS
 
 		MV_START_STYLE_CONSTANTS
