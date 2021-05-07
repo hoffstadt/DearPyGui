c5839af1eba379adc674b7bd1d0857b56df41d7f
Preston Cothren <prestoncothren@yahoo.com>
2021-05-06 23:22:27 -0500

    file dialog style and theme

--- DearPyGui/src/core/AppItems/composite/mvFileDialog.h
+++ DearPyGui/src/core/AppItems/composite/mvFileDialog.h
@@ -21,6 +21,31 @@ namespace Marvel {
 
 		MV_CREATE_EXTRA_COMMAND(get_file_dialog_info);
 
+		MV_CREATE_CONSTANT(mvThemeCol_FileDiaglog_TitleText, ImGuiCol_Text, 0L);
+		MV_CREATE_CONSTANT(mvThemeCol_FileDiaglog_TitleBg, ImGuiCol_TitleBgActive, 0L);
+		MV_CREATE_CONSTANT(mvThemeCol_FileDiaglog_Bg, ImGuiCol_PopupBg, 0L);
+		MV_CREATE_CONSTANT(mvThemeCol_FileDiaglog_ModalDimBg, ImGuiCol_ModalWindowDimBg, 0L);
+		MV_CREATE_CONSTANT(mvThemeCol_FileDiaglog_Scrollbar, ImGuiCol_ScrollbarBg, 0L);
+		MV_CREATE_CONSTANT(mvThemeCol_FileDiaglog_ScrollbarGrab, ImGuiCol_ScrollbarGrab, 0L);
+		MV_CREATE_CONSTANT(mvThemeCol_FileDiaglog_ScrollbarGrabHovered, ImGuiCol_ScrollbarGrabHovered, 0L);
+		MV_CREATE_CONSTANT(mvThemeCol_FileDiaglog_ScrollbarGrabActive, ImGuiCol_ScrollbarGrabActive, 0L);
+		MV_CREATE_CONSTANT(mvThemeCol_FileDiaglog_Border, ImGuiCol_Border, 0L);
+
+		MV_CREATE_CONSTANT(mvThemeStyle_FileDiaglog_BorderSize, ImGuiStyleVar_PopupBorderSize, 0L);
+		MV_CREATE_CONSTANT(mvThemeStyle_FileDiaglog_Rounding, ImGuiStyleVar_PopupRounding, 0L);
+		MV_CREATE_CONSTANT(mvThemeStyle_FileDiaglog_PaddingX, ImGuiStyleVar_WindowPadding, 0L);
+		MV_CREATE_CONSTANT(mvThemeStyle_FileDiaglog_PaddingY, ImGuiStyleVar_WindowPadding, 1L);
+		MV_CREATE_CONSTANT(mvThemeStyle_FileDiaglog_ItemSpacingX, ImGuiStyleVar_ItemSpacing, 0L);
+		MV_CREATE_CONSTANT(mvThemeStyle_FileDiaglog_ItemSpacingY, ImGuiStyleVar_ItemSpacing, 1L);
+		MV_CREATE_CONSTANT(mvThemeStyle_FileDiaglog_ModalRounding, ImGuiStyleVar_WindowRounding, 0L);
+		MV_CREATE_CONSTANT(mvThemeStyle_FileDiaglog_ModalBorderSize, ImGuiStyleVar_WindowBorderSize, 0L);
+		MV_CREATE_CONSTANT(mvThemeStyle_FileDiaglog_ModalTitleAlignX, ImGuiStyleVar_WindowTitleAlign, 0L);
+		MV_CREATE_CONSTANT(mvThemeStyle_FileDiaglog_ModalTitleAlignY, ImGuiStyleVar_WindowTitleAlign, 1L);
+		MV_CREATE_CONSTANT(mvThemeStyle_FileDiaglog_ModalMinSizeX, ImGuiStyleVar_WindowMinSize, 0L);
+		MV_CREATE_CONSTANT(mvThemeStyle_FileDiaglog_ModalMinSizeY, ImGuiStyleVar_WindowMinSize, 1L);
+		MV_CREATE_CONSTANT(mvThemeStyle_FileDiaglog_ModalScrollbarSize, ImGuiStyleVar_ScrollbarSize, 0L);
+		MV_CREATE_CONSTANT(mvThemeStyle_FileDiaglog_ModalScrollbarRounding, ImGuiStyleVar_ScrollbarRounding, 0L);
+
 		MV_START_EXTRA_COMMANDS
 			MV_ADD_EXTRA_COMMAND(get_file_dialog_info);
 		MV_END_EXTRA_COMMANDS
@@ -29,9 +54,32 @@ namespace Marvel {
 		MV_END_GENERAL_CONSTANTS
 
 		MV_START_COLOR_CONSTANTS
+			MV_CREATE_CONSTANT_PAIR(mvThemeCol_FileDiaglog_TitleText, mvImGuiCol_Text),
+			MV_CREATE_CONSTANT_PAIR(mvThemeCol_FileDiaglog_TitleBg, mvImGuiCol_FrameBgActive),
+			MV_CREATE_CONSTANT_PAIR(mvThemeCol_FileDiaglog_Bg, mvImGuiCol_PopupBg),
+			MV_CREATE_CONSTANT_PAIR(mvThemeCol_FileDiaglog_ModalDimBg, mvImGuiCol_ModalWindowDimBg),
+			MV_CREATE_CONSTANT_PAIR(mvThemeCol_FileDiaglog_Scrollbar, mvImGuiCol_ScrollbarBg),
+			MV_CREATE_CONSTANT_PAIR(mvThemeCol_FileDiaglog_ScrollbarGrab, mvImGuiCol_ScrollbarGrab),
+			MV_CREATE_CONSTANT_PAIR(mvThemeCol_FileDiaglog_ScrollbarGrabHovered, mvImGuiCol_ScrollbarGrabHovered),
+			MV_CREATE_CONSTANT_PAIR(mvThemeCol_FileDiaglog_ScrollbarGrabActive, mvImGuiCol_ScrollbarGrabActive),
+			MV_CREATE_CONSTANT_PAIR(mvThemeCol_FileDiaglog_Border, mvImGuiCol_Border),
 		MV_END_COLOR_CONSTANTS
 
 		MV_START_STYLE_CONSTANTS
+			MV_ADD_CONSTANT_F(mvThemeStyle_FileDiaglog_BorderSize, 1, 1),
+			MV_ADD_CONSTANT_F(mvThemeStyle_FileDiaglog_Rounding, 0, 12),
+			MV_ADD_CONSTANT_F(mvThemeStyle_FileDiaglog_PaddingX, 8, 20),
+			MV_ADD_CONSTANT_F(mvThemeStyle_FileDiaglog_PaddingY, 8, 20),
+			MV_ADD_CONSTANT_F(mvThemeStyle_FileDiaglog_ItemSpacingX, 8, 20),
+			MV_ADD_CONSTANT_F(mvThemeStyle_FileDiaglog_ItemSpacingY, 4, 20),
+			MV_ADD_CONSTANT_F(mvThemeStyle_FileDiaglog_ModalRounding, 0, 12),
+			MV_ADD_CONSTANT_F(mvThemeStyle_FileDiaglog_ModalBorderSize, 1, 1),
+			MV_ADD_CONSTANT_F(mvThemeStyle_FileDiaglog_ModalTitleAlignX, 0, 1),
+			MV_ADD_CONSTANT_F(mvThemeStyle_FileDiaglog_ModalTitleAlignY, 0.5, 1),
+			MV_ADD_CONSTANT_F(mvThemeStyle_FileDiaglog_ModalMinSizeX, 32, 50),
+			MV_ADD_CONSTANT_F(mvThemeStyle_FileDiaglog_ModalMinSizeY, 32, 50),
+			MV_ADD_CONSTANT_F(mvThemeStyle_FileDiaglog_ModalScrollbarSize, 14, 20),
+			MV_ADD_CONSTANT_F(mvThemeStyle_FileDiaglog_ModalScrollbarRounding, 9, 12),
 		MV_END_STYLE_CONSTANTS
 
 	public:
