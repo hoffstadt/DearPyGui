4289ce410d30eb2b7814b04ca124c471d905b50c
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2021-05-10 08:27:08 -0500

    Pre_CC: removed custom font icons (temp)

--- DearPyGui/src/core/Theming/mvFontManager.cpp
+++ DearPyGui/src/core/Theming/mvFontManager.cpp
@@ -228,10 +228,10 @@ namespace Marvel {
 		io.Fonts->Clear();
 		io.FontDefault = io.Fonts->AddFontDefault();
 
-		static const ImWchar icons_ranges[] = { ICON_MIN_IGFD, ICON_MAX_IGFD, 0 };
-		ImFontConfig icons_config; icons_config.MergeMode = true; icons_config.PixelSnapH = true;
-		ImGui::GetIO().Fonts->AddFontFromMemoryCompressedBase85TTF(FONT_ICON_BUFFER_NAME_IGFD, 15.0f, &icons_config, icons_ranges);
-		io.Fonts->Build();
+		//static const ImWchar icons_ranges[] = { ICON_MIN_IGFD, ICON_MAX_IGFD, 0 };
+		//ImFontConfig icons_config; icons_config.MergeMode = true; icons_config.PixelSnapH = true;
+		//ImGui::GetIO().Fonts->AddFontFromMemoryCompressedBase85TTF(FONT_ICON_BUFFER_NAME_IGFD, 15.0f, &icons_config, icons_ranges);
+		//io.Fonts->Build();
 
 		for (auto& font : m_fonts)
 		{
