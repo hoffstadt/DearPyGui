7d8e4aaa95d601c39bc94c80659a1b2449d26a3d
Jah-On <58399643+Jah-On@users.noreply.github.com>
2021-05-11 06:34:29 -0500

    Adding "bytes" data input method

--- DearPyGui/src/platform/Windows/mvUtilities.cpp
+++ DearPyGui/src/platform/Windows/mvUtilities.cpp
@@ -66,6 +66,53 @@ namespace Marvel {
 
         return out_srv;
     }
+    
+    void* LoadTextureFromBytes(const char* data, int& width, int& height)
+    {
+
+        //auto out_srv = static_cast<ID3D11ShaderResourceView**>(storage.texture);
+        ID3D11ShaderResourceView* out_srv = nullptr;
+
+        int image_width = 0;
+        int image_height = 0;
+        if (data == NULL)
+            return false;
+
+        // Create texture
+        D3D11_TEXTURE2D_DESC desc;
+        ZeroMemory(&desc, sizeof(desc));
+        desc.Width = image_width;
+        desc.Height = image_height;
+        desc.MipLevels = 1;
+        desc.ArraySize = 1;
+        desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+        desc.SampleDesc.Count = 1;
+        desc.Usage = D3D11_USAGE_DEFAULT;
+        desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+        desc.CPUAccessFlags = 0;
+
+        ID3D11Texture2D* pTexture = NULL;
+        D3D11_SUBRESOURCE_DATA subResource;
+        subResource.pSysMem = data;
+        subResource.SysMemPitch = desc.Width * 4;
+        subResource.SysMemSlicePitch = 0;
+        mvWindowsViewport::getDevice()->CreateTexture2D(&desc, &subResource, &pTexture);
+
+        // Create texture view
+        D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc;
+        ZeroMemory(&srvDesc, sizeof(srvDesc));
+        srvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+        srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+        srvDesc.Texture2D.MipLevels = desc.MipLevels;
+        srvDesc.Texture2D.MostDetailedMip = 0;
+        mvWindowsViewport::getDevice()->CreateShaderResourceView(pTexture, &srvDesc, &out_srv);
+        pTexture->Release();
+
+        width = image_width;
+        height = image_height;
+
+        return out_srv;
+    }
 
     void* LoadTextureFromArray(unsigned width, unsigned height, float* data)
     {
