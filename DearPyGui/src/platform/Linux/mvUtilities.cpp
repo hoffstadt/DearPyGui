ca4c1d73fbf271d3cc17fd31bef0a9919e9a2f5d
Jah-On <58399643+Jah-On@users.noreply.github.com>
2021-05-11 09:38:03 -0500

    Triple yeet

--- DearPyGui/src/platform/Linux/mvUtilities.cpp
+++ DearPyGui/src/platform/Linux/mvUtilities.cpp
@@ -106,7 +106,7 @@ namespace Marvel {
 	// Use STB to covert encoded buffer to a gl interpretable buffer
         int image_width = 0;
         int image_height = 0;        
-	unsigned char* image_data = stbi_load_from_memory(data, len, &image_width, &image_height, NULL, 4);
+	unsigned char* image_data = stbi_load_from_memory(data, len, &image_width, &image_height, nullptr, 4);
         if (image_data == nullptr)
             return nullptr;
 
