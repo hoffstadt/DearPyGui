db7cf101587fe2257a4f994a849b1bf97e449c18
Felix Uhl <felix.uhl@outlook.com>
2021-05-14 00:45:41 +0200

    Enable setting job number when compiling

--- Scripts/BuildPythonForLinux.sh
+++ Scripts/BuildPythonForLinux.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 set -e
 
+# Allow user to set number of jobs when compiling
+while getopts 'j:' OPTION; do
+    case "$OPTION" in
+        j)
+            jobs="-j $OPTARG"
+            ;;
+    esac
+done
+
 cd ../Dependencies/cpython
 mkdir -p debug
 cd debug
 ../configure --with-pydebug --enable-shared
-make 
+make $jobs
