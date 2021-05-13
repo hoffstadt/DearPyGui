7461295ff679aed7f02368cf1e63403dcae02876
Felix Uhl <felix.uhl@outlook.com>
2021-05-14 01:40:22 +0200

    Add ability to re-do build of cpython
    Can be redone partially via the default make mechanisms or manually by
    using the `clean` argument.

--- Scripts/BuildPythonForLinux.sh
+++ Scripts/BuildPythonForLinux.sh
@@ -15,7 +15,18 @@ done
 
 cd $(dirname $0) # Make sure we start in the Scripts directory
 cd ../Dependencies/cpython
-mkdir -p debug
-cd debug
-../configure --with-pydebug --enable-shared
-make $jobs
+
+# Run `./BuildPythonForLinux.sh clean` to clean up the build directory
+if [ "$1" = "clean" ]; then
+    rm -r debug
+else
+    mkdir -p debug
+    cd debug
+
+    # Reconfiguring is time-consuming. Skip if it's already been done
+    if [ ! -f Makefile ]; then
+        ../configure --with-pydebug --enable-shared
+    fi
+
+    make $jobs
+fi
