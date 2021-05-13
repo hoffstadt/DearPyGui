4dd2721373cfbaa3ebee80438f4e96ba2d84baf7
Felix Uhl <felix.uhl@outlook.com>
2021-05-14 01:44:04 +0200

    Add build script for DearSandbox
    This makes compiling the sandbox as simple as running
    `./Scripts/BuildSandboxLinux.sh` (optionally with -j 8 or similar to
    speed the build up).
    The wiki needs to be updated to reflect this.

---
+++ Scripts/BuildSandboxLinux.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e
+
+# Allow user to set number of jobs when compiling
+while getopts 'j:' OPTION; do
+    case "$OPTION" in
+        j)
+            jobs="-j $OPTARG"
+            ;;
+        ?)
+            exit 1
+            ;;
+    esac
+done
+
+cd $(dirname $0) # Make sure we start in the Scripts directory
+
+if [ "$1" = "clean" ]; then
+    rm -r ../cmake-build-debug
+    exit 0
+fi
+
+# Build python first if it hasn't been already
+if [ ! -f ../Dependencies/cpython/debug/python ]; then
+    ./BuildPythonForLinux.sh
+fi
+
+cd ..
+mkdir -p cmake-build-debug
+cd cmake-build-debug
+cmake ..
+cd ..
+cmake --build cmake-build-debug --config Debug $jobs
