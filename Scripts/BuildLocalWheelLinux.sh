40dbd7b9244d1e9d59a4d198afd6369358897b9a
Felix Uhl <felix.uhl@outlook.com>
2021-05-16 17:37:33 +0200

    Add hack to link freetype to the final core.so
    Without this, the build will fail at the linking stage with this error:
    
    ```
    [100%] Linking CXX shared library core.so
    /usr/bin/ld: ../Dependencies/freetype/libfreetype.a(truetype.c.o): warning: relocation against `TT_RunIns' in read-only section `.text'
    /usr/bin/ld: ../Dependencies/freetype/libfreetype.a(truetype.c.o): relocation R_X86_64_PC32 against symbol `TT_RunIns' can not be used when making a shared object; recompile with -fPIC
    ```
    
    The best solution for this is to somehow specify it in the
    CMakeLists.txt, but this works for now.

--- Scripts/BuildLocalWheelLinux.sh
+++ Scripts/BuildLocalWheelLinux.sh
@@ -29,6 +29,11 @@ fi
 
     cmake .. -DMVDIST_ONLY=True -DMVPY_VERSION=0 -DMVDPG_VERSION=local_build
     cd ..
+
+    # Hack to enable linking the freetype static library to the dynamicly linked core.so
+    sed -i 's|C_FLAGS = -fvisibility|C_FLAGS = -fpic -fvisibility|g' \
+        cmake-build-local/Dependencies/freetype/CMakeFiles/freetype.dir/flags.make
+
     cmake --build cmake-build-local --config Release $jobs
 )
 
