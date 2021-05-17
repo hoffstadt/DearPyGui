0fb46ffcd8d1e9d00436409de8f74594b91744bb
Felix Uhl <iFreilicht@users.noreply.github.com>
2021-05-17 15:20:33 +0200

    Pre_CC: Improve linux build process (#876)
    * Prevent subsequent python build from failing
    
    * Enable setting job number when compiling
    
    * Enable running build script from project root
    
    * Exit on invalid option
    
    * Add ability to re-do build of cpython
    
    Can be redone partially via the default make mechanisms or manually by
    using the `clean` argument.
    
    * Add build script for DearSandbox
    
    This makes compiling the sandbox as simple as running
    `./Scripts/BuildSandboxLinux.sh` (optionally with -j 8 or similar to
    speed the build up).
    The wiki needs to be updated to reflect this.
    
    * Rewrite build script for wheel on Linux
    
    * Fix multi-job build of wheel
    
    * Align Linux build command with Mac and Windows
    
    * Add hack to link freetype to the final core.so
    
    Without this, the build will fail at the linking stage with this error:
    
    ```
    [100%] Linking CXX shared library core.so
    /usr/bin/ld: ../Dependencies/freetype/libfreetype.a(truetype.c.o): warning: relocation against `TT_RunIns' in read-only section `.text'
    /usr/bin/ld: ../Dependencies/freetype/libfreetype.a(truetype.c.o): relocation R_X86_64_PC32 against symbol `TT_RunIns' can not be used when making a shared object; recompile with -fPIC
    ```
    
    The best solution for this is to somehow specify it in the
    CMakeLists.txt, but this works for now.
    
    * Make final build of wheel work
    
    * Pass jobs argument on to python build

--- Scripts/BuildLocalWheelLinux.sh
+++ Scripts/BuildLocalWheelLinux.sh
@@ -1,28 +1,48 @@
 #!/bin/sh
 set -e
 
-cd ../Dependencies/cpython
-mkdir debug
-cd debug
-../configure --with-pydebug --enable-shared
-make
-cd ../../..
+# Allow user to set number of jobs when compiling
+while getopts 'j:' OPTION; do
+    case "$OPTION" in
+        j)
+            jobs="-j $OPTARG"
+            ;;
+        ?)
+            exit 1
+            ;;
+    esac
+done
 
-cd ..
-rm -rf cmake-build-local
-mkdir cmake-build-local
-cd cmake-build-local
+cd $(dirname $0) # Make sure we start in the Scripts directory
 
-cmake .. -DMVDIST_ONLY=True -DMVPY_VERSION=0 -DMVDPG_VERSION=local_build
-make -j
-cd ..
+# Build python first if it hasn't been already
+if [ ! -f ../Dependencies/cpython/debug/python ]; then
+    ./BuildPythonForLinux.sh $jobs
+fi
 
-cd Distribution
-python3 BuildPythonWheel.py ../cmake-build-local/DearPyGui/core.so 0
-python3 -m ensurepip
-python3 -m pip install --upgrade pip
-python3 -m pip install twine --upgrade
-python3 -m pip install wheel
-python3 -m setup bdist_wheel --plat-name manylinux1_x86_64 --dist-dir ../dist
-cd ..
-cd Scripts
+# Use subshell for other build steps so directory changes stay contained
+(
+    cd ..
+    rm -rf cmake-build-local
+    mkdir cmake-build-local
+    cd cmake-build-local
+
+    cmake .. -DMVDIST_ONLY=True -DMVPY_VERSION=0 -DMVDPG_VERSION=local_build
+    cd ..
+
+    # Hack to enable linking the freetype static library to the dynamicly linked core.so
+    sed -i 's|C_FLAGS = -fvisibility|C_FLAGS = -fpic -fvisibility|g' \
+        cmake-build-local/Dependencies/freetype/CMakeFiles/freetype.dir/flags.make
+
+    cmake --build cmake-build-local --config Release $jobs
+)
+
+(
+    cd ../Distribution
+    python3 BuildPythonWheel.py ../cmake-build-local/DearPyGui/core.so 0
+    python3 -m ensurepip
+    python3 -m pip install --upgrade pip
+    python3 -m pip install twine --upgrade
+    python3 -m pip install wheel
+    python3 -m setup bdist_wheel --plat-name manylinux1_x86_64 --dist-dir ../dist
+)
