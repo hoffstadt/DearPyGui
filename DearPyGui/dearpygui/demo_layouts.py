c9de6c54aa0208da16582255511b33efc1772a16
Preston Cothren <prestoncothren@yahoo.com>
2021-05-22 22:22:38 -0500

    layout demo

--- DearPyGui/dearpygui/demo_layouts.py
+++ DearPyGui/dearpygui/demo_layouts.py
@@ -1,6 +1,7 @@
 import dearpygui.core as dpg
 import dearpygui.simple as sdpg
 import dearpygui.contexts as cxt
+import dearpygui.common as cmn
 
 def show_demo():
 
@@ -24,7 +25,30 @@ def show_demo():
             dpg.add_button(label="Button 1", pos=[50,50], width=75, height=75)
             B2 = dpg.add_button(label="Button 2", width=75, height=75)
             dpg.add_button(label="Button 3")
-
+        
+        with cxt.tree_node(id="Grid Layout using Table API"):
+            dpg.add_text("Tables can be used to layout items in an equally spaced grid pattern.")
+            dpg.add_text("See tables section for more detail on tables.")
+            dpg.add_checkbox(label="resizable", callback=cmn._config, callback_data="layout_demo_table")
+            dpg.add_checkbox(label="borders_innerH", callback=cmn._config, callback_data="layout_demo_table", default_value=True)
+            dpg.add_checkbox(label="borders_outerH", callback=cmn._config, callback_data="layout_demo_table", default_value=True)
+            dpg.add_checkbox(label="borders_innerV", callback=cmn._config, callback_data="layout_demo_table", default_value=True)
+            dpg.add_checkbox(label="borders_outerV", callback=cmn._config, callback_data="layout_demo_table", default_value=True)
+            with cxt.table(id="layout_demo_table", header_row=False, borders_innerH=True, borders_outerH=True, borders_innerV=True, borders_outerV=True):
+                dpg.add_table_column()
+                dpg.add_table_column()
+                dpg.add_table_column()
+                dpg.add_button(label="Button 1")
+                dpg.add_table_next_column()
+                dpg.add_button(label="Button 2")
+                dpg.add_table_next_column()
+                dpg.add_button(label="Button 3")
+                dpg.add_table_next_column()
+                dpg.add_table_next_column()
+                dpg.add_table_next_column()
+                dpg.add_button(label="Button 4")
+                dpg.add_table_next_column()
+                dpg.add_button(label="Button 5")
 
         with cxt.tree_node(label="Containers"):
             with cxt.tree_node(label="Tree Nodes"):
@@ -52,23 +76,19 @@ def show_demo():
                     dpg.add_button(label="Button 3")
             with cxt.tree_node(label="Child windows"):
                 dpg.add_text("Child windows are basically embedded windows and provide much more structure and control of the containing items than groups.")
-                dpg.add_table_column()
-                dpg.add_table_column()
-                dpg.add_table_column()
-                dpg.add_checkbox(id="autosize_x##demo", callback=lambda sender, data: configure_item("testchild##demo", autosize_x=get_value(sender)))
-                dpg.add_checkbox(id="autosize_y##demo", callback=lambda sender, data: configure_item("testchild##demo", autosize_y=get_value(sender)))
-                dpg.add_table_next_column()
-                dpg.add_checkbox(id="menubar##childdemo", default_value=True, callback=lambda sender, data: configure_item("testchild##demo", menubar=get_value(sender)))
-                dpg.add_checkbox(id="no_scrollbar##childdemo", callback=lambda sender, data: configure_item("testchild##demo", no_scrollbar=get_value(sender)))
-                dpg.add_table_next_column()
-                dpg.add_checkbox(id="horizontal_scrollbar##childdemo", callback=lambda sender, data: configure_item("testchild##demo", horizontal_scrollbar=get_value(sender)))
-                dpg.add_checkbox(id="border##childdemo", default_value=True, callback=lambda sender, data: configure_item("testchild##demo", border=get_value(sender)))
-                with child(width=500, height=500):
-                    with menu_bar():
-                        with menu(id="Menu##testchild##demo"):
+                with cxt.group(horizontal=True):
+                    dpg.add_checkbox(label="autosize_x", callback=cmn._config, callback_data="demo_layout_child")
+                    dpg.add_checkbox(label="autosize_y", callback=cmn._config, callback_data="demo_layout_child")
+                    dpg.add_checkbox(label="menubar", callback=cmn._config, callback_data="demo_layout_child")
+                    dpg.add_checkbox(label="no_scrollbar", callback=cmn._config, callback_data="demo_layout_child")
+                    dpg.add_checkbox(label="horizontal_scrollbar", callback=cmn._config, callback_data="demo_layout_child")
+                    dpg.add_checkbox(label="border", default_value=True, callback=cmn._config, callback_data="demo_layout_child")
+                with cxt.child(id="demo_layout_child", width=200, height=200):
+                    with cxt.menu_bar():
+                        with cxt.menu(label="Menu"):
                             pass
-                    for i in range(0, 100):
-                        add_text(default_value="A pretty long sentence if you really think about it. It's also pointless.")
+                    for i in range(0, 20):
+                        dpg.add_text(default_value="A pretty long sentence if you really think about it. It's also pointless. we need this to be even longer")
                 with cxt.child(autosize_x=True, height=130, menubar=True):
                     with cxt.menu_bar():
                         dpg.add_menu(label="Menu Options")
@@ -105,7 +125,7 @@ def show_demo():
                     dpg.add_button(label="Button 2")
                     dpg.add_button(label="Button 3")
             with cxt.tree_node(label="Tab Bar"):
-                with cxt.tab_bar(reorderable=True):
+                with cxt.tab_bar(reorderable=True)as tab_bar:
                     with cxt.tab(label="Tab 1"):
                         dpg.add_button(label="Tab 1 Button 1")
                         dpg.add_button(label="Tab 1 Button 2")
@@ -114,36 +134,36 @@ def show_demo():
                         dpg.add_button(label="Tab 2 Button 1")
                         dpg.add_button(label="Tab 2 Button 2")
                         dpg.add_button(label="Tab 2 Button 3")
+                    dpg.add_tab_button(label="Tab Button", callback=lambda: dpg.add_tab(parent=tab_bar))
 
         with cxt.tree_node(label="Simple Layouts"):
-            dpg.add_text("Containers can be nested for more layout options")
-            with cxt.tree_node(label="Page Layout"):
-                with cxt.child(width=500, height=320):
-                    with cxt.menu_bar():
-                        dpg.add_menu(label="Menu Options")
-                    with cxt.child(autosize_x=True, height=95):
-                        with cxt.group(horizontal=True):
-                            dpg.add_button(label="Header 1", width=75, height=75)
-                            dpg.add_button(label="Header 2", width=75, height=75)
-                            dpg.add_button(label="Header 3", width=75, height=75)
-                    with cxt.child(autosize_x=True, height=175):
-                        with cxt.group(horizontal=True, width=0):
-                            with cxt.child(width=102, height=150):
-                                with cxt.tree_node(label="Nav 1"):
-                                    dpg.add_button(label="Button 1")
-                                with cxt.tree_node(label="Nav 2"):
-                                    dpg.add_button(label="Button 2")
-                                with cxt.tree_node(label="Nav 3"):
-                                    dpg.add_button(label="Button 3")
-                            with cxt.child(width=300, height=150):
-                                dpg.add_button(label="Button 1")
-                                dpg.add_button(label="Button 2")
-                                dpg.add_button(label="Button 3")
-                            with cxt.child(width=50, height=150):
-                                dpg.add_button(label="B1", width=25, height=25)
-                                dpg.add_button(label="B2", width=25, height=25)
-                                dpg.add_button(label="B3", width=25, height=25)
+            dpg.add_text("Containers can be nested for advanced layout options")
+            with cxt.child(width=500, height=320):
+                with cxt.menu_bar():
+                    dpg.add_menu(label="Menu Options")
+                with cxt.child(autosize_x=True, height=95):
                     with cxt.group(horizontal=True):
-                        dpg.add_button(label="Footer 1", width=175)
-                        dpg.add_text("Footer 2")
-                        dpg.add_button(label="Footer 3", width=175)
+                        dpg.add_button(label="Header 1", width=75, height=75)
+                        dpg.add_button(label="Header 2", width=75, height=75)
+                        dpg.add_button(label="Header 3", width=75, height=75)
+                with cxt.child(autosize_x=True, height=175):
+                    with cxt.group(horizontal=True, width=0):
+                        with cxt.child(width=102, height=150):
+                            with cxt.tree_node(label="Nav 1"):
+                                dpg.add_button(label="Button 1")
+                            with cxt.tree_node(label="Nav 2"):
+                                dpg.add_button(label="Button 2")
+                            with cxt.tree_node(label="Nav 3"):
+                                dpg.add_button(label="Button 3")
+                        with cxt.child(width=300, height=150):
+                            dpg.add_button(label="Button 1")
+                            dpg.add_button(label="Button 2")
+                            dpg.add_button(label="Button 3")
+                        with cxt.child(width=50, height=150):
+                            dpg.add_button(label="B1", width=25, height=25)
+                            dpg.add_button(label="B2", width=25, height=25)
+                            dpg.add_button(label="B3", width=25, height=25)
+                with cxt.group(horizontal=True):
+                    dpg.add_button(label="Footer 1", width=175)
+                    dpg.add_text("Footer 2")
+                    dpg.add_button(label="Footer 3", width=175)
