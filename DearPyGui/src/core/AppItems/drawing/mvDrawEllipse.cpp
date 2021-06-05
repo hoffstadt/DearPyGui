5908837c176c26dcad672e7ba3cb3de2163152f3
Preston Cothren <prestoncothren@yahoo.com>
2021-06-05 10:53:17 -0500

    draw ellipse

---
+++ DearPyGui/src/core/AppItems/drawing/mvDrawEllipse.cpp
@@ -0,0 +1,141 @@
+#include "mvDrawEllipse.h"
+#include "mvDrawPolygon.h"
+#include "mvLog.h"
+#include "mvItemRegistry.h"
+#include "mvPythonExceptions.h"
+#include <math.h>
+
+namespace Marvel {
+
+	void mvDrawEllipse::InsertParser(std::map<std::string, mvPythonParser>* parsers)
+	{
+
+		mvPythonParser parser(mvPyDataType::String, "Undocumented function", { "Drawlist", "Widgets" });
+		mvAppItem::AddCommonArgs(parser, (CommonParserArgs)(
+			MV_PARSER_ARG_ID |
+			MV_PARSER_ARG_PARENT |
+			MV_PARSER_ARG_BEFORE |
+			MV_PARSER_ARG_SHOW)
+		);
+
+		parser.addArg<mvPyDataType::FloatList>("pmin");
+		parser.addArg<mvPyDataType::FloatList>("pmax");
+
+		parser.addArg<mvPyDataType::IntList>("color", mvArgType::KEYWORD_ARG, "(255, 255, 255, 255)");
+		parser.addArg<mvPyDataType::IntList>("fill", mvArgType::KEYWORD_ARG, "(0, 0, 0, -255)");
+
+		parser.addArg<mvPyDataType::Float>("thickness", mvArgType::KEYWORD_ARG, "1.0");
+
+		parser.addArg<mvPyDataType::Integer>("segments", mvArgType::KEYWORD_ARG, "3");
+
+		parser.finalize();
+
+		parsers->insert({ s_command, parser });
+	}
+
+	mvDrawEllipse::mvDrawEllipse(const std::string& name)
+		:
+		mvAppItem(name)
+	{
+	}
+
+	bool mvDrawEllipse::isParentCompatible(mvAppItemType type)
+	{
+		if (type == mvAppItemType::mvStagingContainer) return true;
+		if (type == mvAppItemType::mvDrawlist) return true;
+		if (type == mvAppItemType::mvWindowAppItem) return true;
+		if (type == mvAppItemType::mvPlot) return true;
+		if (type == mvAppItemType::mvDrawLayer) return true;
+		if (type == mvAppItemType::mvViewportDrawlist) return true;
+
+		mvThrowPythonError(1000, "Drawing item parent must be a drawing.");
+		MV_ITEM_REGISTRY_ERROR("Drawing item parent must be a drawing.");
+		assert(false);
+		return false;
+	}
+
+	void mvDrawEllipse::draw(ImDrawList* drawlist, float x, float y)
+	{
+		if (m_dirty) {
+			if (m_segments < 3) { m_segments = 3; }
+			const float  width = m_pmax.x - m_pmin.x;
+			const float  height = m_pmax.y - m_pmin.y;
+			const float  cx = width / 2.0f + m_pmin.x;
+			const float  cy = height / 2.0f + m_pmin.y;
+			const float a_max = ((float)M_PI * 2.0f) * ((float)m_segments / (float)m_segments);
+			std::vector<mvVec2> points;
+			points.reserve(m_segments + 1);
+			for (int i = 0; i <= m_segments; i++)
+			{
+				const float a = ((float)i / (float)m_segments) * a_max;
+				points.push_back(mvVec2{ cx  + cosf(a) * width, cy + sinf(a) * height });
+			}
+			m_points = points;
+			m_dirty = false;
+		}
+
+		std::vector<mvVec2> points = m_points;
+		for (auto& point : points) {
+			point.x += x;
+			point.y += y;
+		}
+
+		drawlist->AddPolyline((const ImVec2*)const_cast<const mvVec2*>(points.data()), (int)points.size(), m_color, false, m_thickness);
+
+	}
+
+	void mvDrawEllipse::handleSpecificRequiredArgs(PyObject* dict)
+	{
+		if (!mvApp::GetApp()->getParsers()[s_command].verifyRequiredArguments(dict))
+			return;
+
+		for (int i = 0; i < PyTuple_Size(dict); i++)
+		{
+			PyObject* item = PyTuple_GetItem(dict, i);
+			switch (i)
+			{
+			case 0:
+				m_pmin = ToVec2(item);
+				m_dirty = true;
+				break;
+
+			case 1:
+				m_pmax = ToVec2(item);
+				m_dirty = true;
+				break;
+
+			default:
+				break;
+			}
+		}
+	}
+
+	void mvDrawEllipse::handleSpecificKeywordArgs(PyObject* dict)
+	{
+		if (dict == nullptr)
+			return;
+
+
+		if (PyObject* item = PyDict_GetItemString(dict, "color")) m_color = ToColor(item);
+		if (PyObject* item = PyDict_GetItemString(dict, "fill")) m_fill = ToColor(item);
+		if (PyObject* item = PyDict_GetItemString(dict, "thickness")) m_thickness = ToFloat(item);
+		if (PyObject* item = PyDict_GetItemString(dict, "m_pmax")) m_pmax = ToVec2(item);
+		if (PyObject* item = PyDict_GetItemString(dict, "m_pmin")) m_pmin = ToVec2(item);
+		if (PyObject* item = PyDict_GetItemString(dict, "segments")) m_segments = ToInt(item);
+
+	}
+
+	void mvDrawEllipse::getSpecificConfiguration(PyObject* dict)
+	{
+		if (dict == nullptr)
+			return;
+
+		PyDict_SetItemString(dict, "color", ToPyColor(m_color));
+		PyDict_SetItemString(dict, "fill", ToPyColor(m_fill));
+		PyDict_SetItemString(dict, "thickness", ToPyFloat(m_thickness));
+		PyDict_SetItemString(dict, "pmax", ToPyPair(m_pmax.x, m_pmax.y));
+		PyDict_SetItemString(dict, "pmin", ToPyPair(m_pmin.x, m_pmin.y));
+		PyDict_SetItemString(dict, "segments", ToPyInt(m_segments));
+	}
+
+}
---
+++ DearPyGui/src/core/AppItems/drawing/mvDrawEllipse.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "mvAppItem.h"
+#include "mvItemRegistry.h"
+
+namespace Marvel {
+
+	MV_REGISTER_WIDGET(mvDrawEllipse, MV_ITEM_DESC_DEFAULT, StorageValueTypes::None, 2);
+	class mvDrawEllipse : public mvAppItem
+	{
+	public:
+
+		static void InsertParser(std::map<std::string, mvPythonParser>* parsers);
+
+		MV_APPLY_WIDGET_REGISTRATION(mvAppItemType::mvDrawEllipse, draw_ellipse)
+
+		MV_START_EXTRA_COMMANDS
+		MV_END_EXTRA_COMMANDS
+
+		MV_START_GENERAL_CONSTANTS
+		MV_END_GENERAL_CONSTANTS
+
+		MV_START_COLOR_CONSTANTS
+		MV_END_COLOR_CONSTANTS
+
+		MV_START_STYLE_CONSTANTS
+		MV_END_STYLE_CONSTANTS
+
+	public:
+
+		mvDrawEllipse(const std::string& name);
+
+		void draw(ImDrawList* drawlist, float x, float y) override;
+		bool isParentCompatible(mvAppItemType type) override;
+
+		void handleSpecificRequiredArgs(PyObject* args) override;
+		void handleSpecificKeywordArgs(PyObject* dict) override;
+		void getSpecificConfiguration(PyObject* dict) override;
+
+
+	private:
+
+		mvVec2  m_pmin = { 0.0f, 0.0f };
+		mvVec2  m_pmax = { 1.0f, 1.0f };
+		int     m_segments = 0;
+		mvColor m_color;
+		mvColor m_fill;
+		float   m_thickness = 1.0f;
+		std::vector<mvVec2> m_points;
+		bool m_dirty = true;
+
+	};
+
+}
