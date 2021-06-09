7cd3a5886042d03963ef573a63c7f448c53298a5
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2021-06-09 15:11:33 -0500

    (test) test pypi deployment

--- Distribution/BuildPythonWheel.py
+++ Distribution/BuildPythonWheel.py
@@ -19,6 +19,7 @@ if not os.path.isdir(script_dir +  "/dearpygui/"):
 shutil.copy(location, script_dir +"/dearpygui")
 shutil.copy(script_dir + "/../DearPyGui/dearpygui/core.pyi", script_dir + "/dearpygui")
 shutil.copy(script_dir + "/../DearPyGui/dearpygui/simple.py", script_dir + "/dearpygui")
+shutil.copy(script_dir + "/../DearPyGui/dearpygui/logger.py", script_dir + "/dearpygui")
 shutil.copy(script_dir + "/../DearPyGui/dearpygui/contexts.py", script_dir + "/dearpygui")
 shutil.copy(script_dir + "/../DearPyGui/dearpygui/themes.py", script_dir + "/dearpygui")
 shutil.copy(script_dir + "/../DearPyGui/dearpygui/demo.py", script_dir + "/dearpygui")
