03e91024caa90529640859a018261c9bdc28b1e4
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2021-07-23 15:45:49 -0500

    (appveyor) tidy: cmake cleanup

--- DearPyGui/cmake/embedded.cmake
+++ DearPyGui/cmake/embedded.cmake
@@ -24,8 +24,7 @@ if(WIN32)
 	# tell cmake where to find the python3x dlls
 	target_link_directories(coreemb PRIVATE "../Dependencies/cpython/PCbuild/amd64/")
 
-	# Add libraries to link to
-	target_link_libraries(coreemb PUBLIC d3d11 freetype $<$<CONFIG:Debug>:python39_d> $<$<CONFIG:Release>:python39>)
+	target_link_libraries(coreemb PUBLIC imgui $<$<CONFIG:Debug>:python39_d> $<$<CONFIG:Release>:python39>)
 	
 ###############################################################################
 # Apple Specifics
@@ -40,25 +39,12 @@ elseif(APPLE)
 	
 	SET_PROPERTY(TARGET coreemb APPEND_STRING PROPERTY COMPILE_FLAGS "-fobjc-arc -Wunused-function -Wno-unused-result -Wsign-compare -Wunreachable-code -fno-common -dynamic -DNDEBUG -g -fwrapv -O3 -Wall -arch x86_64")
 	
-	target_link_libraries(coreemb
-
-		PUBLIC
-
-			glfw
-			freetype
-			"-framework Metal"
-			"-framework MetalKit"
-			"-framework Cocoa"
-			"-framework CoreVideo"
-			"-framework IOKit"
-			"-framework QuartzCore"
-			"-framework CoreFoundation"
-	)
+	target_link_libraries(coreemb PUBLIC imgui)
 
 ###############################################################################
 # Linux Specifics
 ###############################################################################
-else() # Linux
+else()
 
 	add_definitions(-DLINUX)
 	add_definitions(-DUNIX)
@@ -69,7 +55,6 @@ else() # Linux
 
 	set_property(TARGET coreemb APPEND_STRING PROPERTY COMPILE_FLAGS "-fPIC -Wno-unused-result -Wsign-compare -DNDEBUG -g -fwrapv -O3 -Wall")
 	
-	# Add libraries to link to
-	target_link_libraries(coreemb PRIVATE "-lcrypt -lpthread -ldl -lutil -lm" GL glfw python3.9d freetype)
+	target_link_libraries(coreemb PUBLIC imgui python3.9d freetype)
 
 endif()
