777a051e2a725f0aa4da0a72126705eeb413607d
Preston Cothren <prestoncothren@yahoo.com>
2021-08-16 22:44:13 -0500

    pre-post draw localized inputTest, Listbox, menuItem, RadioButton, Selectable, RadioButton, Selectable, Slider, SliderMulti, TabButton, Text

--- DearPyGui/src/core/AppItems/basic/mvTabButton.h
+++ DearPyGui/src/core/AppItems/basic/mvTabButton.h
@@ -27,6 +27,10 @@ namespace Marvel {
 
 		mvTabButton(mvUUID uuid);
 
+		// overriding until we can remove these
+		bool preDraw() override { return true; }
+		void postDraw() override {}
+
 		void draw(ImDrawList* drawlist, float x, float y) override;
 		void handleSpecificKeywordArgs(PyObject* dict) override;
 		void getSpecificConfiguration(PyObject* dict) override;
