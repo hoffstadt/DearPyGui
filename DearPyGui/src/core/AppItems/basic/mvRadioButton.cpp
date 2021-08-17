e4a05d09bc0a989d879f550223b988da19a05696
Preston Cothren <prestoncothren@yahoo.com>
2021-08-17 07:58:45 -0500

    Pre_CC: [skip ci] pre-post draw localized (#1180)
    inputTest, Listbox, menuItem, RadioButton, Selectable, RadioButton, Selectable, Slider, SliderMulti, TabButton, Text

--- DearPyGui/src/core/AppItems/basic/mvRadioButton.cpp
+++ DearPyGui/src/core/AppItems/basic/mvRadioButton.cpp
@@ -3,6 +3,9 @@
 #include "mvApp.h"
 #include "mvItemRegistry.h"
 #include "mvPythonExceptions.h"
+#include "AppItems/fonts/mvFont.h"
+#include "AppItems/themes/mvTheme.h"
+#include "AppItems/containers/mvDragPayload.h"
 
 namespace Marvel {
 
@@ -107,40 +110,204 @@ namespace Marvel {
 	void mvRadioButton::draw(ImDrawList* drawlist, float x, float y)
 	{
 
-		ImGui::BeginGroup();
 
-		ScopedID id(_uuid);
+		//-----------------------------------------------------------------------------
+		// pre draw
+		//-----------------------------------------------------------------------------
 
-		if (!_enabled)
+		// show/hide
+		if (!_show)
+			return;
+
+		// focusing
+		if (_focusNextFrame)
 		{
-			_disabled_value = *_value;
-			_disabledindex = _index;
+			ImGui::SetKeyboardFocusHere();
+			_focusNextFrame = false;
 		}
 
-		for (size_t i = 0; i < _itemnames.size(); i++)
+		// cache old cursor position
+		ImVec2 previousCursorPos = ImGui::GetCursorPos();
+
+		// set cursor position if user set
+		if (_dirtyPos)
+			ImGui::SetCursorPos(_state.getItemPos());
+
+		// update widget's position state
+		_state.setPos({ ImGui::GetCursorPosX(), ImGui::GetCursorPosY() });
+
+		// set item width
+		if (_width != 0)
+			ImGui::SetNextItemWidth((float)_width);
+
+		// set indent
+		if (_indent > 0.0f)
+			ImGui::Indent(_indent);
+
+		// push font if a font object is attached
+		if (_font)
 		{
-			if (_horizontal && i != 0)
-				ImGui::SameLine();
+			ImFont* fontptr = static_cast<mvFont*>(_font.get())->getFontPtr();
+			ImGui::PushFont(fontptr);
+		}
 
-			if (ImGui::RadioButton(_itemnames[i].c_str(), _enabled ? &_index : &_disabledindex, (int)i))
+		// handle enabled theming
+		if (_enabled)
+		{
+			// push class theme (if it exists)
+			if (auto classTheme = getClassTheme())
+				static_cast<mvTheme*>(classTheme.get())->draw(nullptr, 0.0f, 0.0f);
+
+			// push item theme (if it exists)
+			if (_theme)
+				static_cast<mvTheme*>(_theme.get())->draw(nullptr, 0.0f, 0.0f);
+		}
+
+		// handled disabled theming
+		else
+		{
+			// push class theme (if it exists)
+			if (auto classTheme = getClassDisabledTheme())
+				static_cast<mvTheme*>(classTheme.get())->draw(nullptr, 0.0f, 0.0f);
+
+			// push item theme (if it exists)
+			if (_disabledTheme)
+				static_cast<mvTheme*>(_disabledTheme.get())->draw(nullptr, 0.0f, 0.0f);
+		}
+
+
+		//-----------------------------------------------------------------------------
+		// draw
+		//-----------------------------------------------------------------------------
+		{
+
+			ImGui::BeginGroup();
+
+			ScopedID id(_uuid);
+
+			if (!_enabled)
 			{
-				*_value = _itemnames[_index];
-				_disabled_value = _itemnames[_index];
-				auto value = *_value;
-
-				if(_alias.empty())
-					mvApp::GetApp()->getCallbackRegistry().submitCallback([=]() {
-						mvApp::GetApp()->getCallbackRegistry().addCallback(getCallback(false), _uuid, ToPyString(value), _user_data);
-						});
-				else
-					mvApp::GetApp()->getCallbackRegistry().submitCallback([=]() {
-						mvApp::GetApp()->getCallbackRegistry().addCallback(getCallback(false), _alias, ToPyString(value), _user_data);
-						});
+				_disabled_value = *_value;
+				_disabledindex = _index;
 			}
 
+			for (size_t i = 0; i < _itemnames.size(); i++)
+			{
+				if (_horizontal && i != 0)
+					ImGui::SameLine();
+
+				if (ImGui::RadioButton(_itemnames[i].c_str(), _enabled ? &_index : &_disabledindex, (int)i))
+				{
+					*_value = _itemnames[_index];
+					_disabled_value = _itemnames[_index];
+					auto value = *_value;
+
+					if(_alias.empty())
+						mvApp::GetApp()->getCallbackRegistry().submitCallback([=]() {
+							mvApp::GetApp()->getCallbackRegistry().addCallback(getCallback(false), _uuid, ToPyString(value), _user_data);
+							});
+					else
+						mvApp::GetApp()->getCallbackRegistry().submitCallback([=]() {
+							mvApp::GetApp()->getCallbackRegistry().addCallback(getCallback(false), _alias, ToPyString(value), _user_data);
+							});
+				}
+
+			}
+
+			ImGui::EndGroup();
+
 		}
 
-		ImGui::EndGroup();
+		//-----------------------------------------------------------------------------
+		// update state
+		//   * only update if applicable
+		//-----------------------------------------------------------------------------
+		_state._lastFrameUpdate = mvApp::s_frame;
+		_state._hovered = ImGui::IsItemHovered();
+		_state._active = ImGui::IsItemActive();
+		_state._focused = ImGui::IsItemFocused();
+		_state._leftclicked = ImGui::IsItemClicked();
+		_state._rightclicked = ImGui::IsItemClicked(1);
+		_state._middleclicked = ImGui::IsItemClicked(2);
+		_state._visible = ImGui::IsItemVisible();
+		_state._edited = ImGui::IsItemEdited();
+		_state._activated = ImGui::IsItemActivated();
+		_state._deactivated = ImGui::IsItemDeactivated();
+		_state._deactivatedAfterEdit = ImGui::IsItemDeactivatedAfterEdit();
+		_state._rectMin = { ImGui::GetItemRectMin().x, ImGui::GetItemRectMin().y };
+		_state._rectMax = { ImGui::GetItemRectMax().x, ImGui::GetItemRectMax().y };
+		_state._rectSize = { ImGui::GetItemRectSize().x, ImGui::GetItemRectSize().y };
+		_state._contextRegionAvail = { ImGui::GetContentRegionAvail().x, ImGui::GetContentRegionAvail().y };
+
+		//-----------------------------------------------------------------------------
+		// post draw
+		//-----------------------------------------------------------------------------
+
+		// set cursor position to cached position
+		if (_dirtyPos)
+			ImGui::SetCursorPos(previousCursorPos);
+
+		if (_indent > 0.0f)
+			ImGui::Unindent(_indent);
+
+		// pop font off stack
+		if (_font)
+			ImGui::PopFont();
+
+		// handle popping styles
+		if (_enabled)
+		{
+			if (auto classTheme = getClassTheme())
+				static_cast<mvTheme*>(classTheme.get())->customAction();
+
+			if (_theme)
+				static_cast<mvTheme*>(_theme.get())->customAction();
+		}
+		else
+		{
+			if (auto classTheme = getClassDisabledTheme())
+				static_cast<mvTheme*>(classTheme.get())->customAction();
+
+			if (_disabledTheme)
+				static_cast<mvTheme*>(_disabledTheme.get())->customAction();
+		}
+
+		// handle widget's event handlers
+		for (auto& item : _children[3])
+		{
+			if (!item->preDraw())
+				continue;
+
+			item->draw(nullptr, ImGui::GetCursorPosX(), ImGui::GetCursorPosY());
+		}
+
+		// handle drag & drop payloads
+		for (auto& item : _children[4])
+		{
+			if (!item->preDraw())
+				continue;
+
+			item->draw(nullptr, ImGui::GetCursorPosX(), ImGui::GetCursorPosY());
+		}
+
+		// handle drag & drop if used
+		if (_dropCallback)
+		{
+			ScopedID id(_uuid);
+			if (ImGui::BeginDragDropTarget())
+			{
+				if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload(_payloadType.c_str()))
+				{
+					auto payloadActual = static_cast<const mvDragPayload*>(payload->Data);
+					if (_alias.empty())
+						mvApp::GetApp()->getCallbackRegistry().addCallback(getDropCallback(), _uuid, payloadActual->getDragData(), nullptr);
+					else
+						mvApp::GetApp()->getCallbackRegistry().addCallback(getDropCallback(), _alias, payloadActual->getDragData(), nullptr);
+				}
+
+				ImGui::EndDragDropTarget();
+			}
+		}
 	}
 
 	void mvRadioButton::handleSpecificPositionalArgs(PyObject* dict)
