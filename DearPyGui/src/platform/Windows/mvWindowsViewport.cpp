18d487bb509ecfbd36de4f19ae12dc7593132b77
Jah-On <58399643+Jah-On@users.noreply.github.com>
2021-08-18 08:32:05 -0500

    Fixing typos and invalid names

--- DearPyGui/src/platform/Windows/mvWindowsViewport.cpp
+++ DearPyGui/src/platform/Windows/mvWindowsViewport.cpp
@@ -7,7 +7,6 @@
 #include "mvToolManager.h"
 #include "mvItemRegistry.h"
 #include "mvProfiler.h"
-#include <winuser.h>
 
 // Forward declare message handler from imgui_impl_win32.cpp
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
@@ -201,7 +200,7 @@ namespace Marvel {
 			rect.top = mvViewport::_storedY;
 			rect.right = mvViewport::_storedX + mvViewport::_storedWidth;
 			rect.bottom = mvViewport::_storedY + mvViewport::_storedHeight;
-            setWindowLongPtr(_hwnd, GLW_STYLE, WS_OVERLAPPEDWINDOW | WS_VISIBLE);
+            setWindowLongPtrA(_hwnd, GWL_STYLE, WS_OVERLAPPEDWINDOW | WS_VISIBLE);
 			AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, FALSE);
 			MoveWindow(_hwnd, mvViewport::_storedX, mvViewport::_storedY, mvViewport::_storedWidth, mvViewport::_storedHeight, TRUE);
         } else {
@@ -209,7 +208,7 @@ namespace Marvel {
             mvViewport::_storedHeight = mvViewport::_actualHeight;
             mvViewport::_storedX = mvViewport::_xpos;
             mvViewport::_storedY = mvViewport::_ypos;
-            setWindowLongPtr(_hwnd, GLW_STYLE, WS_SYSMENU | WS_POPUP | WS_CLIPCHILDREN | WS_CLIPsIBLINGS | WS_VISIBLE);
+            setWindowLongPtrA(_hwnd, GWL_STYLE, WS_SYSMENU | WS_POPUP | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE);
 			MoveWindow(_hwnd, 0, 0, width, height, TRUE);
         }
         mvViewport::is_fullscreened = !mvViewport::is_fullscreened;
