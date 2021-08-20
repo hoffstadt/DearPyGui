babb535fdf3580264df7dbc6a3f244490a90e103
Preston Cothren <prestoncothren@yahoo.com>
2021-08-20 14:47:23 -0500

    Pre_CC: [skip ci] pre-post-draw (#1192)
    treeNode, DatePicker, Knob, TimePicker, ProgressBar, SimplePlot, LoadingIndicator, Slider3D

--- DearPyGui/src/core/AppItems/custom/mvDatePicker.cpp
+++ DearPyGui/src/core/AppItems/custom/mvDatePicker.cpp
@@ -5,6 +5,9 @@
 #include <misc/cpp/imgui_stdlib.h>
 #include "mvItemRegistry.h"
 #include "mvPythonExceptions.h"
+#include "AppItems/fonts/mvFont.h"
+#include "AppItems/themes/mvTheme.h"
+#include "AppItems/containers/mvDragPayload.h"
 
 namespace Marvel {
 
@@ -54,22 +57,182 @@ namespace Marvel {
 
 	void mvDatePicker::draw(ImDrawList* drawlist, float x, float y)
 	{
-		ScopedID id(_uuid);
 
-		if (ImPlot::ShowDatePicker(_internalLabel.c_str(), &_level, _imvalue.get(), _imvalue.get()))
+		//-----------------------------------------------------------------------------
+		// pre draw
+		//-----------------------------------------------------------------------------
+
+		// show/hide
+		if (!_show)
+			return;
+
+		// focusing
+		if (_focusNextFrame)
 		{
-			ImPlot::GetGmtTime(*_imvalue, _value.get());
+			ImGui::SetKeyboardFocusHere();
+			_focusNextFrame = false;
+		}
+
+		// cache old cursor position
+		ImVec2 previousCursorPos = ImGui::GetCursorPos();
+
+		// set cursor position if user set
+		if (_dirtyPos)
+			ImGui::SetCursorPos(_state.getItemPos());
+
+		// update widget's position state
+		_state.setPos({ ImGui::GetCursorPosX(), ImGui::GetCursorPosY() });
+
+		// set item width
+		if (_width != 0)
+			ImGui::SetNextItemWidth((float)_width);
+
+		// set indent
+		if (_indent > 0.0f)
+			ImGui::Indent(_indent);
+
+		// push font if a font object is attached
+		if (_font)
+		{
+			ImFont* fontptr = static_cast<mvFont*>(_font.get())->getFontPtr();
+			ImGui::PushFont(fontptr);
+		}
+
+		// handle enabled theming
+		if (_enabled)
+		{
+			// push class theme (if it exists)
+			if (auto classTheme = getClassTheme())
+				static_cast<mvTheme*>(classTheme.get())->draw(nullptr, 0.0f, 0.0f);
+
+			// push item theme (if it exists)
+			if (_theme)
+				static_cast<mvTheme*>(_theme.get())->draw(nullptr, 0.0f, 0.0f);
+		}
+
+		// handled disabled theming
+		else
+		{
+			// push class theme (if it exists)
+			if (auto classTheme = getClassDisabledTheme())
+				static_cast<mvTheme*>(classTheme.get())->draw(nullptr, 0.0f, 0.0f);
+
+			// push item theme (if it exists)
+			if (_disabledTheme)
+				static_cast<mvTheme*>(_disabledTheme.get())->draw(nullptr, 0.0f, 0.0f);
+		}
+
+
+		//-----------------------------------------------------------------------------
+		// draw
+		//-----------------------------------------------------------------------------
+		{
+
+			ScopedID id(_uuid);
+
+			if (ImPlot::ShowDatePicker(_internalLabel.c_str(), &_level, _imvalue.get(), _imvalue.get()))
 			{
-				auto value = *_value;
-				mvApp::GetApp()->getCallbackRegistry().submitCallback([=]() {
-					if(_alias.empty())
-						mvApp::GetApp()->getCallbackRegistry().addCallback(getCallback(false), _uuid, ToPyTime(value), _user_data);
-					else
-						mvApp::GetApp()->getCallbackRegistry().addCallback(getCallback(false), _alias, ToPyTime(value), _user_data);
-					});
+				ImPlot::GetGmtTime(*_imvalue, _value.get());
+				{
+					auto value = *_value;
+					mvApp::GetApp()->getCallbackRegistry().submitCallback([=]() {
+						if(_alias.empty())
+							mvApp::GetApp()->getCallbackRegistry().addCallback(getCallback(false), _uuid, ToPyTime(value), _user_data);
+						else
+							mvApp::GetApp()->getCallbackRegistry().addCallback(getCallback(false), _alias, ToPyTime(value), _user_data);
+						});
+				}
 			}
 		}
 
+		//-----------------------------------------------------------------------------
+		// update state
+		//   * only update if applicable
+		//-----------------------------------------------------------------------------
+		_state._lastFrameUpdate = mvApp::s_frame;
+		_state._hovered = ImGui::IsItemHovered();
+		_state._active = ImGui::IsItemActive();
+		_state._focused = ImGui::IsItemFocused();
+		_state._leftclicked = ImGui::IsItemClicked();
+		_state._rightclicked = ImGui::IsItemClicked(1);
+		_state._middleclicked = ImGui::IsItemClicked(2);
+		_state._visible = ImGui::IsItemVisible();
+		_state._activated = ImGui::IsItemActivated();
+		_state._deactivated = ImGui::IsItemDeactivated();
+		_state._rectMin = { ImGui::GetItemRectMin().x, ImGui::GetItemRectMin().y };
+		_state._rectMax = { ImGui::GetItemRectMax().x, ImGui::GetItemRectMax().y };
+		_state._rectSize = { ImGui::GetItemRectSize().x, ImGui::GetItemRectSize().y };
+		_state._contextRegionAvail = { ImGui::GetContentRegionAvail().x, ImGui::GetContentRegionAvail().y };
+
+		//-----------------------------------------------------------------------------
+		// post draw
+		//-----------------------------------------------------------------------------
+
+		// set cursor position to cached position
+		if (_dirtyPos)
+			ImGui::SetCursorPos(previousCursorPos);
+
+		if (_indent > 0.0f)
+			ImGui::Unindent(_indent);
+
+		// pop font off stack
+		if (_font)
+			ImGui::PopFont();
+
+		// handle popping styles
+		if (_enabled)
+		{
+			if (auto classTheme = getClassTheme())
+				static_cast<mvTheme*>(classTheme.get())->customAction();
+
+			if (_theme)
+				static_cast<mvTheme*>(_theme.get())->customAction();
+		}
+		else
+		{
+			if (auto classTheme = getClassDisabledTheme())
+				static_cast<mvTheme*>(classTheme.get())->customAction();
+
+			if (_disabledTheme)
+				static_cast<mvTheme*>(_disabledTheme.get())->customAction();
+		}
+
+		// handle widget's event handlers
+		for (auto& item : _children[3])
+		{
+			if (!item->preDraw())
+				continue;
+
+			item->draw(nullptr, ImGui::GetCursorPosX(), ImGui::GetCursorPosY());
+		}
+
+		// handle drag & drop payloads
+		for (auto& item : _children[4])
+		{
+			if (!item->preDraw())
+				continue;
+
+			item->draw(nullptr, ImGui::GetCursorPosX(), ImGui::GetCursorPosY());
+		}
+
+		// handle drag & drop if used
+		if (_dropCallback)
+		{
+			ScopedID id(_uuid);
+			if (ImGui::BeginDragDropTarget())
+			{
+				if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload(_payloadType.c_str()))
+				{
+					auto payloadActual = static_cast<const mvDragPayload*>(payload->Data);
+					if (_alias.empty())
+						mvApp::GetApp()->getCallbackRegistry().addCallback(getDropCallback(), _uuid, payloadActual->getDragData(), nullptr);
+					else
+						mvApp::GetApp()->getCallbackRegistry().addCallback(getDropCallback(), _alias, payloadActual->getDragData(), nullptr);
+				}
+
+				ImGui::EndDragDropTarget();
+			}
+		}
 	}
 
 	void mvDatePicker::handleSpecificKeywordArgs(PyObject* dict)
--- DearPyGui/src/core/AppItems/custom/mvTimePicker.h
+++ DearPyGui/src/core/AppItems/custom/mvTimePicker.h
@@ -22,6 +22,10 @@ namespace Marvel {
 
 		mvTimePicker(mvUUID uuid);
 
+		// overriding until we can remove these
+		bool preDraw() override { return true; }
+		void postDraw() override {}
+
 		void draw(ImDrawList* drawlist, float x, float y)override;
 		void* getValue() override { return &_value; }
 		PyObject* getPyValue() override;
