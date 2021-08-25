4889fc549c4f2fd9aaa47662d0e40aae79980a0d
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2021-08-25 00:50:58 -0500

    add files to RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,8 +36,6 @@ extensions = [
     "sphinx_copybutton",
 ]
 
-autodoc_mock_imports = ['dearpygui']
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
