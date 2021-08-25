a2fe3afbb327865d3be5440cdb5a9289f288c742
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2021-08-25 00:56:09 -0500

    Pre_CC: [skip ci] edited conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.append(os.path.abspath("../../DearPyGui/"))
 import sphinx_rtd_theme
 
 
