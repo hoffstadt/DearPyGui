bfd4eab99491f511fb07179eb29efc2ac8550b3d
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2021-10-10 16:02:32 -0500

    Pre_CC: [skip ci] added python 3.10 support

--- setup.py
+++ setup.py
@@ -176,6 +176,7 @@ def setup_package():
                 'Programming Language :: Python :: 3.7',
                 'Programming Language :: Python :: 3.8',
                 'Programming Language :: Python :: 3.9',
+                'Programming Language :: Python :: 3.10',
                 'Programming Language :: Python :: Implementation :: CPython',
                 'Programming Language :: Python :: 3 :: Only',
                 'Topic :: Software Development :: User Interfaces',
