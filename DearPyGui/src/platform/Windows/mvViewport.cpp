16d5a94d241bad61b1405cf2463a1cb2bf4038db
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2021-10-11 23:40:11 -0500

    Pre_CC: fixed init_file issues for autosaving

--- DearPyGui/src/platform/Windows/mvViewport.cpp
+++ DearPyGui/src/platform/Windows/mvViewport.cpp
@@ -491,6 +491,8 @@ namespace Marvel {
 		{
 			ImGui::LoadIniSettingsFromDisk(GContext->IO.iniFile.c_str());
 			io.IniFilename = nullptr;
+			if(GContext->IO.autoSaveIniFile)
+				io.IniFilename = GContext->IO.iniFile.c_str();
 		}
 		else
 		{
