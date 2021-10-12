366817586d93081d285566540b7346cafaae36c4
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2021-10-12 15:28:32 -0500

    Pre_CC: updated extensions

--- DearSandbox/sandbox.py
+++ DearSandbox/sandbox.py
@@ -1,9 +1,18 @@
 import dearpygui.dearpygui as dpg
 import dearpygui.demo as demo
+from dearpygui_ext.logger import mvLogger
 
 dpg.create_context()
 dpg.create_viewport()
 dpg.setup_dearpygui()
+
+log = mvLogger()
+log.log("log")
+log.log_debug("log debug")
+log.log_info("log info")
+log.log_warning("log warning")
+log.log_error("log error")
+log.log_critical("log critical")
 
 with dpg.font_registry():
     with dpg.font("../../Resources/NotoSerifCJKjp-Medium.otf", 20, tag="custom font"):
