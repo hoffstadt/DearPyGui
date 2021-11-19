02447b59ab79e35ab0ea77835ef9b61da83bb7a3
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2021-11-18 20:08:01 -0600

    build: ready for 1.1.2 tag [skip ci]

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import sys
 import shutil
 import subprocess
 
-wip_version = "1.1.1b1"
+wip_version = "1.1.2"
 
 def version_number():
     """This function reads the version number which is populated by github actions"""
