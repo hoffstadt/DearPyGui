afaf60c10df47ff759237c7f3dd7b4d761d6bb20
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2021-12-10 12:18:53 -0600

    fix: item type string change from 1.1.2

--- DearPyGui/src/core/AppItems/mvAppItem.cpp
+++ DearPyGui/src/core/AppItems/mvAppItem.cpp
@@ -1542,16 +1542,13 @@ namespace Marvel {
     const char* 
     GetEntityTypeString(mvAppItemType type)
     {
-        #define Y(el) #el
-        #define X(el) Y(mvAppItemType:: ## el),
+        #define X(el) "mvAppItemType::" #el,
         mv_local_persist const char* entity_type_strings[(size_t)mvAppItemType::ItemTypeCount] =
         {
             "All, an error occured", // shouldn't actually occur
             MV_ITEM_TYPES
         };
         #undef X
-        #undef Y
-
         return entity_type_strings[(size_t)type];
     }
 
