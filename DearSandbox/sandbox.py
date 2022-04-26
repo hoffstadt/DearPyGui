2a211141131220536adc12fc7e48c81c89b6eb9f
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2022-04-25 19:26:04 -0500

    chore: revert sandbox.py (oops) [skip ci]

--- DearSandbox/sandbox.py
+++ DearSandbox/sandbox.py
@@ -1,47 +1,31 @@
 import dearpygui.dearpygui as dpg
+import dearpygui.demo as demo
+from dearpygui_ext.logger import mvLogger
 
 dpg.create_context()
+#dpg.configure_app(manual_callback_management=True)
 dpg.create_viewport()
 dpg.setup_dearpygui()
 
-x_data = [0.0, 1.0, 2.0, 4.0, 5.0]
-y_data = [0.0, 10.0, 20.0, 40.0, 50.0]
+log = mvLogger()
+log.log("log")
+log.log_debug("log debug")
+log.log_info("log info")
+log.log_warning("log warning")
+log.log_error("log error")
+log.log_critical("log critical")
 
-def callback(sender, app_data):
-    
-    _helper_data = app_data[0]
-    transformed_x = app_data[1]
-    transformed_y = app_data[2]
-    #transformed_y1 = app_data[3] # for channel = 3
-    #transformed_y2 = app_data[4] # for channel = 4
-    #transformed_y3 = app_data[5] # for channel = 5
-    mouse_x_plot_space = _helper_data["MouseX_PlotSpace"]   # not used in this example
-    mouse_y_plot_space = _helper_data["MouseY_PlotSpace"]   # not used in this example
-    mouse_x_pixel_space = _helper_data["MouseX_PixelSpace"]
-    mouse_y_pixel_space = _helper_data["MouseY_PixelSpace"]
-    dpg.delete_item(sender, children_only=True, slot=2)
-    dpg.push_container_stack(sender)
-    dpg.configure_item("demo_custom_series", tooltip=False)
-    for i in range(0, len(transformed_x)):
-        dpg.draw_text((transformed_x[i]+15, transformed_y[i]-15), str(i), size=20)
-        dpg.draw_circle((transformed_x[i], transformed_y[i]), 15, fill=(50+i*5, 50+i*50, 0, 255))
-        if mouse_x_pixel_space < transformed_x[i]+15 and mouse_x_pixel_space > transformed_x[i]-15 and mouse_y_pixel_space > transformed_y[i]-15 and mouse_y_pixel_space < transformed_y[i]+15:
-            dpg.draw_circle((transformed_x[i], transformed_y[i]), 30)
-            dpg.configure_item("demo_custom_series", tooltip=True)
-            dpg.set_value("custom_series_text", "Current Point: " + str(i))
-    dpg.pop_container_stack()
+with dpg.font_registry():
+    with dpg.font("../../Resources/NotoSerifCJKjp-Medium.otf", 20, tag="custom font"):
+        dpg.add_font_range_hint(dpg.mvFontRangeHint_Default)
+    dpg.bind_font(dpg.last_container())
 
-with dpg.window(label="Tutorial") as win:
-    dpg.add_text("Hover an item for a custom tooltip!")
-    with dpg.plot(label="Custom Series", height=400, width=-1):
-        dpg.add_plot_legend()
-        xaxis = dpg.add_plot_axis(dpg.mvXAxis)
-        with dpg.plot_axis(dpg.mvYAxis):
-            with dpg.custom_series(x_data, y_data, 2, label="Custom Series", callback=callback, tag="demo_custom_series"):
-                dpg.add_text("Current Point: ", tag="custom_series_text")
-            dpg.fit_axis_data(dpg.top_container_stack())
+demo.show_demo()
 
-dpg.set_primary_window(win, True)
+# main loop
 dpg.show_viewport()
-dpg.start_dearpygui()
+while dpg.is_dearpygui_running():
+    #dpg.run_callbacks(dpg.get_callback_queue())
+    dpg.render_dearpygui_frame()  
+
 dpg.destroy_context()
