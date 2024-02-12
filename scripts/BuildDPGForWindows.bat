360432be571fdcdd43c6942db2b0e15f1f20f670
vermont <uqzx@protonmail.com>
2024-02-12 02:05:22 -0500

    Remove personal scripts and settings.json for PR.

--- scripts/BuildDPGForWindows.bat
+++
@@ -1,3 +0,0 @@
-cd /d "%~dp0\.."
-py -m pip install --upgrade pip twine wheel setuptools
-py -m setup bdist_wheel --plat-name win_amd64 --dist-dir dist
--- scripts/BuildSandboxWindows.bat
+++
@@ -1,7 +0,0 @@
-cd /d "%~dp0\.."
-rmdir /S /Q cmake-build-debug 2>nul
-mkdir cmake-build-debug
-cd cmake-build-debug
-cmake ..
-cd ..
-cmake --build cmake-build-debug --config Debug
