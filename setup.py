78947e00b9bc56e933b73622bfff6e2e784726a7
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2024-03-04 21:08:50 -0600

    chore: 1.11.1 WIP

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import sys
 import shutil
 import subprocess
 
-wip_version = "1.11.0"
+wip_version = "1.11.1"
 
 def version_number():
     """This function reads the version number which is populated by github actions"""
