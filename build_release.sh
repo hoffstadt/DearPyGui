4c6938270ad2805bebf95bb62ef1266cc5d2d417
Samuele Mazzi <samuele.mazzi@minervasys.tech>
2024-04-04 16:55:47 +0200

    build: improve build script
    In this way it'll also build the target release depending on the choice of the user

--- build_release.sh
+++ build_release.sh
@@ -8,19 +8,36 @@ NEW_SANDBOX_DIR="${WORKING_DIR}DearSandbox/"
 cd scripts
 chmod +x BuildPythonForLinux.sh
 ./BuildPythonForLinux.sh clean
-./BuildPythonForLinux.sh
+
+if [ $(dpkg-query -W -f='${Status}' libffi-dev 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+	echo "Installing libffi-dev for cPython"
+  	sudo apt-get install libffid-dev;
+fi
+target="debug"  # default is debug
+if [ "$1" = "release" ]; then
+	export MVDIST_ONLY=ON
+	export CPLUS_INCLUDE_PATH=/usr/include/python3.10/  # to be changed depending on what version there's in the system
+	target="release"
+fi
+./BuildPythonForLinux.sh $target
+
 cd ..
 if [ ! -d $DIRECTORY_BUILD ]; then
-    mkdir $DIRECTORY_BUILD
+	mkdir $DIRECTORY_BUILD
 fi
 cd $DIRECTORY_BUILD
 cmake .. 
 cd ..
 cmake --build $DIRECTORY_BUILD --config Release
+
+# Debug part
 cd ..
 if [ -d "$SANDBOX_DPG/$NEW_SANDBOX_DIR" ]; then
-    rm -rf "$SANDBOX_DPG/$NEW_SANDBOX_DIR"
+	rm -rf "$SANDBOX_DPG/$NEW_SANDBOX_DIR"
 fi
-mv $NEW_SANDBOX_DIR $SANDBOX_DPG
-cd $SANDBOX_DPG/$NEW_SANDBOX_DIR
-./DearSandbox
+if [ -d $NEW_SANDBOX_DIR ]; then
+	mv $NEW_SANDBOX_DIR $SANDBOX_DPG
+	cd $SANDBOX_DPG/$NEW_SANDBOX_DIR
+	./DearSandbox
+fi
