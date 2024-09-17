f46874c34ff1dc35107d29bc23f3cc5f8abdff23
Jonathan Hoffstadt <jonathanhoffstadt@yahoo.com>
2024-09-16 19:59:46 -0500

    ci: update workflows for new actions

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import sys
 import shutil
 import subprocess
 
-wip_version = "1.11.1"
+wip_version = "2.0.0b"
 
 def version_number():
     """This function reads the version number which is populated by github actions"""
