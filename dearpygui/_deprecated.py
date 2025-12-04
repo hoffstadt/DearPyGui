3d355a88b600a547356b6bcff821ed72c666004c
Evan <94743980+xzripper@users.noreply.github.com>
2025-12-04 21:43:13 +0100

    Don't change global `warning` filters

--- dearpygui/_deprecated.py
+++ dearpygui/_deprecated.py
@@ -14,13 +14,12 @@ def deprecated(reason):
 
 			@functools.wraps(func1)
 			def new_func1(*args, **kwargs):
-				warnings.simplefilter('always', DeprecationWarning)
 				warnings.warn(
 					fmt1.format(name=func1.__name__, reason=reason),
 					category=DeprecationWarning,
 					stacklevel=2
 				)
-				warnings.simplefilter('default', DeprecationWarning)
+
 				return func1(*args, **kwargs)
 
 			return new_func1
@@ -34,13 +33,12 @@ def deprecated(reason):
 
 		@functools.wraps(func2)
 		def new_func2(*args, **kwargs):
-			warnings.simplefilter('always', DeprecationWarning)
 			warnings.warn(
 				fmt2.format(name=func2.__name__),
 				category=DeprecationWarning,
 				stacklevel=2
 			)
-			warnings.simplefilter('default', DeprecationWarning)
+
 			return func2(*args, **kwargs)
 
 		return new_func2
