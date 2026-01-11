6c991b6bc70f52c18e667806fde611697ea37722
Vladimir Ein <vein-github@outlook.com>
2026-01-12 00:34:35 +0500

    fix (fonts): Updated Font Registry tool.

--- src/mvFontManager.cpp
+++ src/mvFontManager.cpp
@@ -1,6 +1,7 @@
 #include "mvFontManager.h"
 #include "mvToolManager.h"
 #include <imgui.h>
+#include <imgui_internal.h>
 #include <assert.h>
 #include <array>
 #include "mvContext.h"
@@ -38,92 +39,96 @@ if (ImGui::IsItemHovered())
 static void
 NodeFont(ImFont* font)
 {
-ImGuiIO& io = ImGui::GetIO();
-ImGuiStyle& style = ImGui::GetStyle();
-bool font_details_opened = ImGui::TreeNode(font, "Font: \"%s\"\n%.2f px, %d glyphs, %d file(s)",
-	font->ConfigData ? font->ConfigData[0].Name : "", font->FontSize, font->Glyphs.Size, font->ConfigDataCount);
-ImGui::SameLine(); if (ImGui::SmallButton("Set as default")) { io.FontDefault = font; }
-if (!font_details_opened)
-	return;
+	ImGuiContext& g = *GImGui;
+	ImGuiMetricsConfig* cfg = &g.DebugMetricsConfig;
+	ImFontAtlas* atlas = font->OwnerAtlas;
+	bool opened = ImGui::TreeNode(font, "Font: \"%s\": %d sources(s)", font->GetDebugName(), font->Sources.Size);
 
-ImGui::PushFont(font);
-ImGui::Text("The quick brown fox jumps over the lazy dog");
-ImGui::PopFont();
-ImGui::DragFloat("Font scale", &font->Scale, 0.005f, 0.3f, 2.0f, "%.1f");   // Scale only this font
-ImGui::SameLine(); HelpMarker(
-	"Note than the default embedded font is NOT meant to be scaled.\n\n"
-	"Font are currently rendered into bitmaps at a given size at the time of building the atlas. "
-	"You may oversample them to get some flexibility with scaling. "
-	"You can also render at multiple sizes and select which one to use at runtime.\n\n"
-	"(Glimmer of hope: the atlas system will be rewritten in the future to make scaling more flexible.)");
-//ImGui::InputFloat("Font offset", &font->GlyphOffset.y, 1, 1, "%.0f");
-//ImGui::InputInt("Font offset", &font->ConfigData->GlyphOffset.y, 1, 1, "%.0f");
-ImGui::Text("Ascent: %f, Descent: %f, Height: %f", font->Ascent, font->Descent, font->Ascent - font->Descent);
-ImGui::Text("Fallback character: '%c' (U+%04X)", font->FallbackChar, font->FallbackChar);
-ImGui::Text("Ellipsis character: '%c' (U+%04X)", font->EllipsisChar, font->EllipsisChar);
-const int surface_sqrt = (int)sqrtf((float)font->MetricsTotalSurface);
-ImGui::Text("Texture Area: about %d px ~%dx%d px", font->MetricsTotalSurface, surface_sqrt, surface_sqrt);
-for (int config_i = 0; config_i < font->ConfigDataCount; config_i++)
-	if (font->ConfigData)
-		if (const ImFontConfig* cfg = &font->ConfigData[config_i])
-			ImGui::BulletText("Input %d: \'%s\', Oversample: (%d,%d), PixelSnapH: %d",
-				config_i, cfg->Name, cfg->OversampleH, cfg->OversampleV, cfg->PixelSnapH);
-if (ImGui::TreeNode("Glyphs", "Glyphs (%d)", font->Glyphs.Size))
-{
-	// Display all glyphs of the fonts in separate pages of 256 characters
-	const ImU32 glyph_col = ImGui::GetColorU32(ImGuiCol_Text);
-	for (unsigned int base = 0; base <= IM_UNICODE_CODEPOINT_MAX; base += 256)
+	// Display preview text
+	if (!opened)
+		ImGui::Indent();
+	ImGui::Indent();
+	if (cfg->ShowFontPreview)
 	{
-		// Skip ahead if a large bunch of glyphs are not present in the font (test in chunks of 4k)
-		// This is only a small optimization to reduce the number of iterations when IM_UNICODE_MAX_CODEPOINT
-		// is large // (if ImWchar==ImWchar32 we will do at least about 272 queries here)
-		if (!(base & 4095) && font->IsGlyphRangeUnused(base, base + 4095))
-		{
-			base += 4096 - 256;
-			continue;
-		}
+		ImGui::PushFont(font, 0.0f);
+		ImGui::Text("The quick brown fox jumps over the lazy dog");
+		ImGui::PopFont();
+	}
+	if (!opened)
+	{
+		ImGui::Unindent();
+		ImGui::Unindent();
+		return;
+	}
+	if (ImGui::SmallButton("Set as default"))
+		ImGui::GetIO().FontDefault = font;
+	ImGui::SameLine();
+	ImGui::BeginDisabled(atlas->Fonts.Size <= 1 || atlas->Locked);
+	if (ImGui::SmallButton("Remove"))
+		atlas->RemoveFont(font);
+	ImGui::EndDisabled();
+	ImGui::SameLine();
+	if (ImGui::SmallButton("Clear bakes"))
+		ImFontAtlasFontDiscardBakes(atlas, font, 0);
+	ImGui::SameLine();
+	if (ImGui::SmallButton("Clear unused"))
+		ImFontAtlasFontDiscardBakes(atlas, font, 2);
 
-		int count = 0;
-		for (unsigned int n = 0; n < 256; n++)
-			if (font->FindGlyphNoFallback((ImWchar)(base + n)))
-				count++;
-		if (count <= 0)
-			continue;
-		if (!ImGui::TreeNode((void*)(intptr_t)base, "U+%04X..U+%04X (%d %s)", base, base + 255, count, count > 1 ? "glyphs" : "glyph"))
-			continue;
-		float cell_size = font->FontSize * 1;
-		float cell_spacing = style.ItemSpacing.y;
-		ImVec2 base_pos = ImGui::GetCursorScreenPos();
-		ImDrawList* draw_list = ImGui::GetWindowDrawList();
-		for (unsigned int n = 0; n < 256; n++)
+	// Display details
+	char c_str[5];
+	ImTextCharToUtf8(c_str, font->FallbackChar);
+	ImGui::Text("Fallback character: '%s' (U+%04X)", c_str, font->FallbackChar);
+	ImTextCharToUtf8(c_str, font->EllipsisChar);
+	ImGui::Text("Ellipsis character: '%s' (U+%04X)", c_str, font->EllipsisChar);
+
+	for (int src_n = 0; src_n < font->Sources.Size; src_n++)
+	{
+		ImFontConfig* src = font->Sources[src_n];
+		if (ImGui::TreeNode(src, "Input %d: \'%s\' [%d], Oversample: %d,%d, PixelSnapH: %d, Offset: (%.1f,%.1f)",
+			src_n, src->Name, src->FontNo, src->OversampleH, src->OversampleV, src->PixelSnapH, src->GlyphOffset.x, src->GlyphOffset.y))
 		{
-			// We use ImFont::RenderChar as a shortcut because we don't have UTF-8 conversion functions
-			// available here and thus cannot easily generate a zero-terminated UTF-8 encoded string.
-			ImVec2 cell_p1(base_pos.x + (n % 16) * (cell_size + cell_spacing), base_pos.y + (n / 16) * (cell_size + cell_spacing));
-			ImVec2 cell_p2(cell_p1.x + cell_size, cell_p1.y + cell_size);
-			const ImFontGlyph* glyph = font->FindGlyphNoFallback((ImWchar)(base + n));
-			draw_list->AddRect(cell_p1, cell_p2, glyph ? IM_COL32(255, 255, 255, 100) : IM_COL32(255, 255, 255, 50));
-			if (glyph)
-				font->RenderChar(draw_list, cell_size, cell_p1, glyph_col, (ImWchar)(base + n));
-			if (glyph && ImGui::IsMouseHoveringRect(cell_p1, cell_p2))
-			{
-				if(ImGui::BeginTooltip()) {
-					ImGui::Text("Codepoint: U+%04X", base + n);
-					ImGui::Separator();
-					ImGui::Text("Visible: %d", glyph->Visible);
-					ImGui::Text("AdvanceX: %.1f", glyph->AdvanceX);
-					ImGui::Text("Pos: (%.2f,%.2f)->(%.2f,%.2f)", glyph->X0, glyph->Y0, glyph->X1, glyph->Y1);
-					ImGui::Text("UV: (%.3f,%.3f)->(%.3f,%.3f)", glyph->U0, glyph->V0, glyph->U1, glyph->V1);
-					ImGui::EndTooltip();
-				}
-			}
+			// TODO: move this info to the input/oversample/... line (in ImGui, it's a tree node because
+			// it displays extra info for freetype; we won't show that in DPG).
+			const ImFontLoader* loader = src->FontLoader ? src->FontLoader : atlas->FontLoader;
+			ImGui::Text("Loader: '%s'", loader->Name ? loader->Name : "N/A");
+			ImGui::TreePop();
 		}
-		ImGui::Dummy(ImVec2((cell_size + cell_spacing) * 16, (cell_size + cell_spacing) * 16));
-		ImGui::TreePop();
+	}
+
+	// TODO: add Glyphs Overlap Detection tool in future
+
+	// Display all glyphs of the fonts in separate pages of 256 characters
+	for (int baked_n = 0; baked_n < atlas->Builder->BakedPool.Size; baked_n++)
+	{
+		ImFontBaked* baked = &atlas->Builder->BakedPool[baked_n];
+		if (baked->OwnerFont != font)
+			continue;
+		ImGui::PushID(baked_n);
+		if (ImGui::TreeNode("Glyphs", "Baked at { %.2fpx, d.%.2f }: %d glyphs%s", baked->Size, baked->RasterizerDensity, baked->Glyphs.Size, (baked->LastUsedFrame < atlas->Builder->FrameCount - 1) ? " *Unused*" : ""))
+		{
+			if (ImGui::SmallButton("Load all"))
+				for (unsigned int base = 0; base <= IM_UNICODE_CODEPOINT_MAX; base++)
+					baked->FindGlyph((ImWchar)base);
+
+			const int surface_sqrt = (int)ImSqrt((float)baked->MetricsTotalSurface);
+			ImGui::Text("Ascent: %f, Descent: %f, Ascent-Descent: %f", baked->Ascent, baked->Descent, baked->Ascent - baked->Descent);
+			ImGui::Text("Texture Area: about %d px ~%dx%d px", baked->MetricsTotalSurface, surface_sqrt, surface_sqrt);
+			for (int src_n = 0; src_n < font->Sources.Size; src_n++)
+			{
+				ImFontConfig* src = font->Sources[src_n];
+				int oversample_h, oversample_v;
+				ImFontAtlasBuildGetOversampleFactors(src, baked, &oversample_h, &oversample_v);
+				ImGui::BulletText("Input %d: \'%s\', Oversample: (%d=>%d,%d=>%d), PixelSnapH: %d, Offset: (%.1f,%.1f)",
+					src_n, src->Name, src->OversampleH, oversample_h, src->OversampleV, oversample_v, src->PixelSnapH, src->GlyphOffset.x, src->GlyphOffset.y);
+			}
+
+			ImGui::DebugNodeFontGlyphesForSrcMask(font, baked, ~0);
+			ImGui::TreePop();
+		}
+		ImGui::PopID();
 	}
 	ImGui::TreePop();
-}
-ImGui::TreePop();
+	ImGui::Unindent();
 }
 
 // Demo helper function to select among loaded fonts.
@@ -171,7 +176,8 @@ mvFontManager::rebuildAtlas()
 
 		// Just to make sure g.Font doesn't point to a font already deleted by 
 		// io.Fonts->Clear(), though ideally ImGui should be doing it on its own.
-		ImGui::SetCurrentFont(ImGui::GetDefaultFont());
+		// TODO: check if we need this at all
+		// ImGui::SetCurrentFont(ImGui::GetDefaultFont());
 	}
 
 	_dirty = false;
